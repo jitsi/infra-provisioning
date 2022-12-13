502455ed5ec5ae367257c982eca217d6557a6987
Aaron van Meerten <aaron.van.meerten@8x8.com>
2022-12-13 08:50:45 -0600

    include standalone terraform

---
+++ terraform/standalone/delete-standalone-server-oracle.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#!/usr/bin/env bash
+set -x
+unset SSH_USER
+
+# IF THE CURRENT DIRECTORY HAS stack-env.sh THEN INCLUDE IT
+[ -e ./stack-env.sh ] && . ./stack-env.sh
+
+# e.g. ../all/bin/terraform/haproxy-shards
+LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
+
+if [ -z $ENVIRONMENT ]; then
+  echo "No ENVIRONMENT provided or found. Exiting..."
+  exit 201
+fi
+
+[ -z "$ROLE" ] && ROLE="haproxy"
+[ -z "$NAME" ] && NAME="$ENVIRONMENT-$ORACLE_REGION-$ROLE"
+[ -z "$ORACLE_GIT_BRANCH" ] && ORACLE_GIT_BRANCH="master"
+
+[ -e "../all/clouds/oracle.sh" ] && . ../all/clouds/oracle.sh
+
+if [ -z "$ORACLE_REGION" ]; then
+  echo "No ORACLE_REGION found. Exiting..."
+  exit 203
+fi
+
+ORACLE_CLOUD_NAME="$ORACLE_REGION-$ENVIRONMENT-oracle"
+[ -e "../all/clouds/${ORACLE_CLOUD_NAME}.sh" ] && . ../all/clouds/${ORACLE_CLOUD_NAME}.sh
+
+
+[ -z "$S3_PROFILE" ] && S3_PROFILE="oracle"
+[ -z "$S3_STATE_BUCKET" ] && S3_STATE_BUCKET="tf-state-$ENVIRONMENT"
+[ -z "$S3_ENDPOINT" ] && S3_ENDPOINT="https://fr4eeztjonbe.compat.objectstorage.$ORACLE_REGION.oraclecloud.com"
+[ -z "$S3_STATE_KEY" ] && S3_STATE_KEY="$ENVIRONMENT/standalone/$UNIQUE_ID/terraform.tfstate"
+
+LOCAL_KEY="terraform-standalone.tfstate"
+
+oci os object get --bucket-name $S3_STATE_BUCKET --name $S3_STATE_KEY --region $ORACLE_REGION --file $LOCAL_KEY
+if [ $? -eq 0 ]; then
+
+    TERRAFORM_MAJOR_VERSION=$(terraform -v | head -1  | awk '{print $2}' | cut -d'.' -f1)
+    TF_GLOBALS_CHDIR=
+    if [[ "$TERRAFORM_MAJOR_VERSION" == "v1" ]]; then
+      TF_GLOBALS_CHDIR="-chdir=$LOCAL_PATH/destroy"
+      TF_CLI_ARGS=""
+      TF_POST_PARAMS=
+    else
+      TF_POST_PARAMS="$LOCAL_PATH/destroy"
+    fi
+    # The —reconfigure option disregards any existing configuration, preventing migration of any existing state
+    terraform $TF_GLOBALS_CHDIR init \
+    -backend-config="bucket=$S3_STATE_BUCKET" \
+    -backend-config="key=$S3_STATE_KEY" \
+    -backend-config="region=$ORACLE_REGION" \
+    -backend-config="profile=$S3_PROFILE" \
+    -backend-config="endpoint=$S3_ENDPOINT" \
+    -reconfigure $TF_POST_PARAMS
+
+    terraform $TF_GLOBALS_CHDIR apply \
+    -var="oracle_region=$ORACLE_REGION" \
+    -var="tenancy_ocid=$TENANCY_OCID" \
+    -auto-approve $TF_POST_PARAMS
+
+else
+  echo "Standalone STACK failed to be deleted, not found"
+  exit 12
+fi
+
