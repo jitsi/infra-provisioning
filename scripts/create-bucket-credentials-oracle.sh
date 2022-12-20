132e5cf6d5cd9a55d3514d4fbada91e8a3c27b8b
Aaron van Meerten <aaron.van.meerten@8x8.com>
2022-12-20 14:29:26 -0600

    bucket credentials

--- scripts/create-bucket-credentials-oracle.sh
+++ scripts/create-bucket-credentials-oracle.sh
@@ -13,10 +13,29 @@ fi
 
 [ -z "$VAULT_PASSWORD_FILE" ] && VAULT_PASSWORD_FILE='./.vault-password.txt'
 
-if [ ! -f "$VAULT_PASSWORD_FILE" ] && [ ! -z "$ANSIBLE_VAULT_PASSWORD_VALUE" ]
-echo "$ANSIBLE_VAULT_PASSWORD_VALUE" > $VAULT_PASSWORD_FILE
-
 [ -z "$BUCKET_NAME" ] && BUCKET_NAME="jvb-bucket-${ENVIRONMENT}"
 
-os object put --region $ORACLE_REGION --bucket $BUCKET_NAME --name id_rsa --file $USER_PRIVATE_KEY_PATH
-os object put --region $ORACLE_REGION --bucket $BUCKET_NAME --name vault-password --file $VAULT_PASSWORD_FILE
+if [ ! -f "$VAULT_PASSWORD_FILE" ] && [ ! -z "$ANSIBLE_VAULT_PASSWORD_VALUE" ]; then
+    echo "$ANSIBLE_VAULT_PASSWORD_VALUE" > $VAULT_PASSWORD_FILE
+fi
+
+if [ ! -f "$VAULT_PASSWORD_FILE" ]; then
+    echo "No VAULT_PASSWORD_FILE found and no ANSIBLE_VAULT_PASSWORD_VALUE set. Exiting..."
+  exit 203
+fi
+
+if [ -z "$ORACLE_REGIONS" ]; then
+  echo "No ORACLE_REGIONS found. Exiting..."
+  exit 203
+fi
+
+if [ -z "$USER_PRIVATE_KEY_PATH" ]
+  echo "No USER_PRIVATE_KEY_PATH found. Exiting..."
+  exit 203
+fi
+
+
+for ORACLE_REGION in $ORACLE_REGIONS; do
+    os object put --region $ORACLE_REGION --bucket $BUCKET_NAME --name id_rsa --file $USER_PRIVATE_KEY_PATH
+    os object put --region $ORACLE_REGION --bucket $BUCKET_NAME --name vault-password --file $VAULT_PASSWORD_FILE
+done
