2b8e32a8324d92107a455fec658c30189658f010
Aaron van Meerten <aaron.van.meerten@8x8.com>
2022-12-20 13:14:02 -0600

    skip termination for jumpbox

--- terraform/jumpbox-oracle/configure-jumpbox-local-oracle.sh
+++ terraform/jumpbox-oracle/configure-jumpbox-local-oracle.sh
@@ -3,6 +3,7 @@
 # default_provision takes the following variables ANSIBLE_PLAYBOOK, ANSIBLE_VARS and HOST_ROLE (defaults to $SHARD_ROLE)
 export ANSIBLE_PLAYBOOK="configure-jumpbox.yml"
 export HOST_ROLE="ssh"
+export SKIP_TERMINATION="true"
 
 . /usr/local/bin/oracle_cache.sh
 export MY_HOSTNAME="${CLOUD_NAME}-${HOST_ROLE}.oracle.infra.jitsi.net"
