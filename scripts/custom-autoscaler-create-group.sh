6f602cebd0646d787a849aec44c8073bb3e1edaf
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-01-04 13:02:42 -0600

    include autoscaler create script

---
+++ scripts/custom-autoscaler-create-group.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+
+set -x
+
+#IF THE CURRENT DIRECTORY HAS stack-env.sh THEN INCLUDE IT
+[ -e ./stack-env.sh ] && . ./stack-env.sh
+
+if [ -z "$ENVIRONMENT" ]; then
+  echo "No ENVIRONMENT found. Exiting..."
+  exit 203
+fi
+
+[ -e ./sites/$ENVIRONMENT/stack-env.sh ] && . ./sites/$ENVIRONMENT/stack-env.sh
+
+#pull in cloud-specific variables, e.g. tenancy
+[ -e "../all/clouds/oracle.sh" ] && . ../all/clouds/oracle.sh
+
+if [ -z "$ORACLE_REGION" ]; then
+  echo "No ORACLE_REGION found.  Exiting..."
+  exit 203
+fi
+
+ORACLE_CLOUD_NAME="$ORACLE_REGION-$ENVIRONMENT-oracle"
+[ -e "../all/clouds/${ORACLE_CLOUD_NAME}.sh" ] && . ../all/clouds/"${ORACLE_CLOUD_NAME}".sh
+
+if [ -z "$SIDECAR_ENV_VARIABLES" ]; then
+  echo "No SIDECAR_ENV_VARIABLES provided or found. Exiting.. "
+  exit 211
+fi
+
+[ -z "$TOKEN" ] && TOKEN=$(JWT_ENV_FILE="/etc/jitsi/autoscaler-sidecar/$SIDECAR_ENV_VARIABLES" /opt/jitsi/jitsi-autoscaler-sidecar/scripts/jwt.sh)
+
+if [ -z "$AUTOSCALER_URL" ]; then
+  echo "No AUTOSCALER_URL provided or found. Exiting.. "
+  exit 212
+fi
+
+if [ -z "$TYPE" ]; then
+  echo "No TYPE provided or found. Exiting.. "
+  exit 213
+fi
+
+if [ -z "$GROUP_NAME" ]; then
+  echo "No GROUP_NAME provided or found. Exiting.. "
+  exit 214
+fi
+
+if [ -z "$INSTANCE_CONFIGURATION_ID" ]; then
+  echo "No INSTANCE_CONFIGURATION_ID provided or found. Exiting.. "
+  exit 215
+fi
+
+if [ -z "$ENABLE_AUTO_SCALE" ]; then
+  echo "No ENABLE_AUTO_SCALE provided or found. Exiting.. "
+  exit 216
+fi
+
+if [ -z "$ENABLE_LAUNCH" ]; then
+  echo "No ENABLE_LAUNCH provided or found. Exiting.. "
+  exit 217
+fi
+
+if [ -z "$ENABLE_SCHEDULER" ]; then
+  echo "No ENABLE_SCHEDULER provided or found. Exiting.. "
+  exit 217
+fi
+
+if [ -z "$ENABLE_RECONFIGURATION" ]; then
+  echo "No ENABLE_RECONFIGURATION provided or found. Exiting.. "
+  exit 217
+fi
+
+if [ -z "$GRACE_PERIOD_TTL_SEC" ]; then
+  echo "No GRACE_PERIOD_TTL_SEC provided or found. Exiting.. "
+  exit 218
+fi
+
+if [ -z "$PROTECTED_TTL_SEC" ]; then
+  echo "No PROTECTED_TTL_SEC provided or found. Exiting.. "
+  exit 219
+fi
+
+if [ -z "$MIN_COUNT" ]; then
+  echo "No MIN_COUNT provided or found. Exiting.. "
+  exit 220
+fi
+
+if [ -z "$MAX_COUNT" ]; then
+  echo "No MAX_COUNT provided or found. Exiting.. "
+  exit 221
+fi
+
+if [ -z "$DESIRED_COUNT" ]; then
+  echo "No DESIRED_COUNT provided or found. Exiting.. "
+  exit 222
+fi
+
+if [ -z "$SCALING_INCREASE_RATE" ]; then
+  echo "No SCALING_INCREASE_RATE provided or found. Exiting.. "
+  exit 223
+fi
+
+if [ -z "$SCALING_DECREASE_RATE" ]; then
+  echo "No SCALING_DECREASE_RATE provided or found. Exiting.. "
+  exit 224
+fi
+
+if [ -z "$SCALE_UP_THRESHOLD" ]; then
+  echo "No SCALE_UP_THRESHOLD provided or found. Exiting.. "
+  exit 225
+fi
+
+if [ -z "$SCALE_DOWN_THRESHOLD" ]; then
+  echo "No SCALE_DOWN_THRESHOLD provided or found. Exiting.. "
+  exit 226
+fi
+
+if [ -z "$SCALE_PERIOD" ]; then
+  echo "No SCALE_PERIOD provided or found. Exiting.. "
+  exit 227
+fi
+
+if [ -z "$SCALE_UP_PERIODS_COUNT" ]; then
+  echo "No SCALE_UP_PERIODS_COUNT provided or found. Exiting.. "
+  exit 228
+fi
+
+if [ -z "$SCALE_DOWN_PERIODS_COUNT" ]; then
+  echo "No SCALE_DOWN_PERIODS_COUNT provided or found. Exiting.. "
+  exit 229
+fi
+
+if [ -z "$CLOUD_PROVIDER" ]; then
+  echo "No CLOUD_PROVIDER provided or found. Exiting.. "
+  exit 230
+fi
+
+REQUEST_BODY='{
+            "name": "'"$GROUP_NAME"'",
+            "type": "'$TYPE'",
+            "region": "'"$ORACLE_REGION"'",
+            "environment": "'"$ENVIRONMENT"'",
+            "compartmentId": "'"$COMPARTMENT_OCID"'",
+            "instanceConfigurationId": "'"$INSTANCE_CONFIGURATION_ID"'",
+            "enableAutoScale": '$ENABLE_AUTO_SCALE',
+            "enableLaunch": '$ENABLE_LAUNCH',
+            "enableScheduler": '$ENABLE_SCHEDULER',
+            "enableReconfiguration": '$ENABLE_RECONFIGURATION',
+            "gracePeriodTTLSec": '$GRACE_PERIOD_TTL_SEC',
+            "protectedTTLSec": '$PROTECTED_TTL_SEC',
+            "scalingOptions": {
+                "minDesired": '$MIN_COUNT',
+                "maxDesired": '$MAX_COUNT',
+                "desiredCount": '$DESIRED_COUNT',
+                "scaleUpQuantity": '$SCALING_INCREASE_RATE',
+                "scaleDownQuantity": '$SCALING_DECREASE_RATE',
+                "scaleUpThreshold": '$SCALE_UP_THRESHOLD',
+                "scaleDownThreshold": '$SCALE_DOWN_THRESHOLD',
+                "scalePeriod": '$SCALE_PERIOD',
+                "scaleUpPeriodsCount": '$SCALE_UP_PERIODS_COUNT',
+                "scaleDownPeriodsCount": '$SCALE_DOWN_PERIODS_COUNT'
+            },
+            "tags":{
+              "release_number": "'"$TAG_RELEASE_NUMBER"'"
+            },
+            "cloud": "'$CLOUD_PROVIDER'"
+}'
+
+echo "Creating group named $GROUP_NAME"
+instanceGroupCreateResponse=$(curl -s -w "\n %{http_code}" -X PUT \
+  "$AUTOSCALER_URL"/groups/"$GROUP_NAME" \
+  -H 'Content-Type: application/json' \
+  -H "Authorization: Bearer $TOKEN" \
+  -d "$REQUEST_BODY")
+
+createGroupHttpCode=$(tail -n1 <<<"$instanceGroupCreateResponse" | sed 's/[^0-9]*//g')
+if [ "$createGroupHttpCode" == 200 ]; then
+  echo "Group $GROUP_NAME was created successfully"
+else
+  echo "Error creating group $GROUP_NAME. AutoScaler response status code is $createGroupHttpCode"
+  exit 205
+fi
