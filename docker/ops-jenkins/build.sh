773a2a58121f1c9cbe7e0c623aa446e71e849534
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-01-12 10:42:41 -0600

    update users

---
+++ docker/ops-jenkins/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# last tag 0.0.1
+# CACHE_FLAG="--no-cache"
+docker buildx build $CACHE_FLAG --platform=linux/arm64,linux/amd64 --push --pull --progress=plain --tag aaronkvanmeerten/ops-jenkins:$TAG --tag aaronkvanmeerten/ops-jenkins:latest .
