972e4ddacf2e41cc010a14c1f83b6aaf9419b215
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-01-16 12:11:38 -0600

    submodule in postinstall lib

--- terraform/lib/postinstall-lib.sh
+++ terraform/lib/postinstall-lib.sh
@@ -128,10 +128,12 @@ function checkout_repos() {
   git clone $INFRA_CUSTOMIZATIONS_REPO $BOOTSTRAP_DIRECTORY/infra-customizations
   cd $BOOTSTRAP_DIRECTORY/infra-configuration
   git checkout $GIT_BRANCH
+  git submodule update --init --recursive
   git show-ref heads/$GIT_BRANCH
   cd -
   cd $BOOTSTRAP_DIRECTORY/infra-customizations
   git checkout $GIT_BRANCH
+  git submodule update --init --recursive
   git show-ref heads/$GIT_BRANCH
   cp -a $BOOTSTRAP_DIRECTORY/infra-customizations/* $BOOTSTRAP_DIRECTORY/infra-configuration
   cd -
@@ -154,6 +156,8 @@ function run_ansible_playbook() {
     if [ $status_code -eq 1 ]; then
         echo 'Provisioning stage failed' > $tmp_msg_file;
     fi
+
+    cd -
     return $status_code
 }
 
