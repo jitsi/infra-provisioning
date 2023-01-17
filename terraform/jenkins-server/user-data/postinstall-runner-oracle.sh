4910aea0a6249b99b63d19b0c25f73eeae078264
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-01-17 16:24:23 -0600

    jenkins server terraform

---
+++ terraform/jenkins-server/user-data/postinstall-runner-oracle.sh
@@ -0,0 +1,19 @@
+export DUMP_COMMAND="dump"
+export PROVISION_COMMAND="provisioning"
+
+function dump() {
+  default_dump
+#  sudo /usr/local/bin/dump-standalone.sh
+}
+
+function provisioning() {
+  local status_code=0
+  
+  $TIMEOUT_BIN $PROVISIONING_TIMEOUT echo "provisioning jenkins server" >>/var/log/bootstrap.log 2>&1 || status_code=1
+
+  if [ $status_code -eq 1 ]; then
+    echo 'Provisioning stage failed' > $tmp_msg_file;
+  fi
+
+  return $status_code;
+}
