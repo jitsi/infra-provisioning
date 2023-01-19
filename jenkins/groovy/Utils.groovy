90b7ba0595968c24074d8a33f145ab4d6ca787f9
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-01-18 21:39:46 -0600

    name with capitals

--- jenkins/groovy/Utils.groovy
+++ jenkins/groovy/Utils.groovy
@@ -1,4 +1,4 @@
-def replicateImageOracle() {
+def ReplicateImageOracle() {
     sh(
         script: """#!/bin/bash
         export FORCE_BUILD_IMAGE=true
@@ -8,14 +8,31 @@ def replicateImageOracle() {
         exit \$?"""
     )
 }
-def setupOCI() {
+def SetupOCI() {
     sh 'rm -rf ~/.oci'
     sh 'mkdir -p ~/.oci'
     sh 'cp "$OCI_CLI_CONFIG_FILE" ~/.oci/config'
     sh 'cp "$OCI_CLI_KEY_FILE" ~/.oci/private-key.pem'
 }
-def setupAnsible() {
+def SetupAnsible() {
     sh 'echo "$ANSIBLE_VAULT_PASSWORD_PATH" > ./.vault-password.txt'
 }
-
+def CheckSkipBuild(image_type, environment) {
+    echo "checking for existing images before building, FORCE_BUILD_IMAGE is ${env.FORCE_BUILD_IMAGE}"
+    def checkOutput = sh(
+        returnStdout: true,
+        script: """#!/bin/bash
+        export IMAGE_TYPE="${image_type}"
+        export CLOUDS=\$(scripts/release_clouds.sh ${environment})
+        SKIP_BUILD=false
+        if [[ "\$FORCE_BUILD_IMAGE" != "true" ]]; then
+            scripts/check-build-oracle-image-for-clouds.sh 1>&2 && SKIP_BUILD=true
+        fi
+        if \$SKIP_BUILD; then
+            echo 'skip'
+        fi"""
+    ).trim()
+    echo checkOutput;
+    return (checkOutput == 'skip');
+}
 return this
