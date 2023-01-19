952b5aee3cb217a3565f41a29e7158a6ccbbb12b
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-01-19 10:49:52 -0600

    generic oracle build support

--- jenkins/groovy/Utils.groovy
+++ jenkins/groovy/Utils.groovy
@@ -27,15 +27,15 @@ def SetupAnsible() {
         sh 'echo "$ANSIBLE_VAULT_PASSWORD_PATH" > ./.vault-password.txt'
     }
 }
-def CheckSkipBuild(image_type, environment) {
-    echo "checking for existing ${image_type} images before building, FORCE_BUILD_IMAGE is ${env.FORCE_BUILD_IMAGE}"
+def CheckSkipBuild(image_type, environment, force_build) {
+    echo "checking for existing ${image_type} images before building, FORCE_BUILD_IMAGE is ${force_build}"
     def checkOutput = sh(
         returnStdout: true,
         script: """#!/bin/bash
         export IMAGE_TYPE="${image_type}"
         export CLOUDS=\$(scripts/release_clouds.sh ${environment})
         SKIP_BUILD=false
-        if [[ "\$FORCE_BUILD_IMAGE" != "true" ]]; then
+        if [[ "${$force_build}" != "true" ]]; then
             scripts/check-build-oracle-image-for-clouds.sh 1>&2 && SKIP_BUILD=true
         fi
         if \$SKIP_BUILD; then
@@ -64,5 +64,56 @@ def SetupRepos(branch) {
   }
 }
 
+def CreateImageOracle(image_type) {
+    def image_script
+    switch (env.IMAGE_TYPE) {
+        case 'JVB':
+            image_script = 'build-jvb-oracle.sh'
+        break;
+        case 'Signal':
+            image_script = 'build-signal-oracle.sh'
+        break;
+        case 'JavaJibri':
+            image_script = 'build-java-jibri-oracle.sh'
+        break;
+        case 'Jigasi':
+            image_script = 'build-jigasi-oracle.sh'
+        break;
+        case 'CoTURN':
+            image_script = 'build-coturn-oracle.sh'
+        break;
+        case 'SeleniumGrid':
+            image_script = 'build-selenium-grid-oracle.sh'
+        break;
+        case 'FocalBase':
+            image_script = 'build-focal-base-oracle.sh'
+        break;
+        case 'JammyBase':
+            image_script = 'build-jammy-base-oracle.sh'
+        break;
+        default:
+            echo "No known image type ${env.IMAGE_TYPE}"
+        break;
+    }
+
+    if (image_script == false) {
+        error("Build failed because no known image script")
+    } else {
+        echo "Running build script ${image_script} for type ${image_type}"
+        sh(
+            script: """#!/bin/bash
+            export CLOUDS=\$(scripts/release_clouds.sh \$ENVIRONMENT)
+            export FORCE_BUILD_IMAGE=true
+            export ANSIBLE_FORCE_COLOR=True
+            scripts/${image_script} ubuntu
+            if [ \$? -eq 0 ]; then
+                echo "Image creation successful"
+            else
+                echo "Failed to create image, skipping replication"
+                exit 2
+            fi"""
+        )
+    }
+}
 
 return this
