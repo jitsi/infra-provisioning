fb3b76803836904ad7889615744298964255258a
Scott Boone <scott.boone@8x8.com>
2023-01-23 14:41:06 -0600

    syntax

--- jenkins/groovy/Utils.groovy
+++ jenkins/groovy/Utils.groovy
@@ -56,7 +56,7 @@ def SetupRepos(branch) {
           try {
             checkout([$class: 'GitSCM', branches: [[name: "origin/${branch}"]], extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: false, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], userRemoteConfigs: [[credentialsId: 'video-infra', url: env.INFRA_CONFIGURATION_REPO]]])
           } catch (hudson.AbortException e) {
-            if e.toString().contains('Couldn\'t find any revision to build') {
+            if (e.toString().contains('Couldn\'t find any revision to build')) {
                 echo 'WARNING: couldn\'t find branch ${branch} in infra-configuration repo, falling back to main'
                 checkout([$class: 'GitSCM', branches: [[name: "origin/main"]], extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: false, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], userRemoteConfigs: [[credentialsId: 'video-infra', url: env.INFRA_CONFIGURATION_REPO]]])
             }
@@ -67,7 +67,7 @@ def SetupRepos(branch) {
           try {
             git branch: branch, url: env.INFRA_CUSTOMIZATIONS_REPO, credentialsId: 'video-infra'
           } catch (hudson.AbortException e) {
-            if e.toString().contains('Couldn\'t find any revision to build') {
+            if (e.toString().contains('Couldn\'t find any revision to build')) {
                 echo "WARNING: couldn't find branch ${branch} in infra-customization repo, falling back to main"
                 git branch: 'main', url: env.INFRA_CUSTOMIZATIONS_REPO, credentialsId: 'video-infra'
             }
