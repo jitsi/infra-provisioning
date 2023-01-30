bf8617ae06f0be114516d54edb5c9bdcb5628e1f
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-01-30 15:25:29 -0600

    coturn terraform stack

---
+++ terraform/create-coturn-stack/create-coturn-stack-oracle.sh
@@ -0,0 +1,168 @@
+#!/bin/bash
+set -x #echo on
+#!/usr/bin/env bash
+unset SSH_USER
+
+#IF THE CURRENT DIRECTORY HAS stack-env.sh THEN INCLUDE IT
+[ -e ./stack-env.sh ] && . ./stack-env.sh
+
+if [ -z "$ENVIRONMENT" ]; then
+  echo "No ENVIRONMENT found. Exiting..."
+  exit 203
+fi
+
+[ -e ./sites/$ENVIRONMENT/stack-env.sh ] && . ./sites/$ENVIRONMENT/stack-env.sh
+
+LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
+
+#load cloud defaults
+[ -e $LOCAL_PATH/../../clouds/all.sh ] && . $LOCAL_PATH/../../clouds/all.sh
+
+# We need an envirnment "all"
+if [ -z "$ENVIRONMENT" ]; then
+  echo "No Environment provided or found. Exiting .."
+  exit 202
+fi
+
+#pull in cloud-specific variables, e.g. tenancy
+[ -e "$LOCAL_PATH/../../clouds/oracle.sh" ] && . $LOCAL_PATH/../../clouds/oracle.sh
+
+if [ -z "$ORACLE_REGION" ]; then
+  echo "No ORACLE_REGION found.  Exiting..."
+  exit 203
+fi
+
+ORACLE_CLOUD_NAME="$ORACLE_REGION-$ENVIRONMENT-oracle"
+[ -e "$LOCAL_PATH/../../clouds/${ORACLE_CLOUD_NAME}.sh" ] && . $LOCAL_PATH/../../clouds/${ORACLE_CLOUD_NAME}.sh
+
+[ -z "$SHAPE" ] && SHAPE="$DEFAULT_COTURN_SHAPE"
+[ -z "$OCPUS" ] && OCPUS=8
+[ -z "$MEMORY_IN_GBS" ] && MEMORY_IN_GBS=16
+
+[ -z "$USER_PUBLIC_KEY_PATH" ] && USER_PUBLIC_KEY_PATH=~/.ssh/id_ed25519.pub
+
+#required 8x8 tag
+[ -z "$SERVICE" ] && SERVICE="$DOMAIN"
+[ -z "$SERVICE" ] && SERVICE="jitsi-coturn"
+
+#Look up images based on version, or default to latest
+[ -z "$COTURN_IMAGE_OCID" ] && COTURN_IMAGE_OCID=$($LOCAL_PATH/../scripts/oracle_custom_images.py --type coTURN --version "latest" --region="$ORACLE_REGION" --compartment_id="$COMPARTMENT_OCID" --tag_namespace="$TAG_NAMESPACE")
+
+#No image was found, probably not built yet?
+if [ -z "$COTURN_IMAGE_OCID" ]; then
+  echo "No COTURN_IMAGE_OCID provided or found. Exiting.. "
+  exit 210
+fi
+
+[ -z "$DESIRED_CAPACITY" ] && DESIRED_CAPACITY="2"
+
+[ -z "$ORACLE_GIT_BRANCH" ] && ORACLE_GIT_BRANCH="master"
+
+# e.g. AVAILABILITY_DOMAINS='[  "ObqI:EU-FRANKFURT-1-AD-1", "ObqI:EU-FRANKFURT-1-AD-2", "ObqI:EU-FRANKFURT-1-AD-3" ]'
+[ -z "$AVAILABILITY_DOMAINS" ] && AVAILABILITY_DOMAINS=$(oci iam availability-domain list --region=$ORACLE_REGION | jq .data[].name | jq --slurp .)
+if [ -z "$AVAILABILITY_DOMAINS" ]; then
+  echo "No AVAILABILITY_DOMAINS found.  Exiting..."
+  exit 206
+fi
+
+VCN_NAME_ROOT="$ORACLE_REGION-$ENVIRONMENT"
+VCN_NAME="$VCN_NAME_ROOT-vcn"
+
+SHARD_ROLE="coturn"
+[ -z "$NAME" ] && NAME="$ENVIRONMENT-$ORACLE_REGION-coturn"
+
+# run as user
+if [ -z "$1" ]; then
+  SSH_USER=$(whoami)
+  echo "Ansible SSH user is not defined. We use current user: $SSH_USER"
+else
+  SSH_USER=$1
+  echo "Run ansible as $SSH_USER"
+fi
+
+[ -z "$USER_PUBLIC_KEY_PATH" ] && USER_PUBLIC_KEY_PATH="~/.ssh/id_ed25519.pub"
+[ -z "$USER_PRIVATE_KEY_PATH" ] && USER_PRIVATE_KEY_PATH="~/.ssh/id_ed25519"
+[ -z "$BASTION_HOST" ] && BASTION_HOST="$CONNECTION_SSH_BASTION_HOST"
+[ -z "$COTURNS_POSTINSTALL_STATUS_FILE" ] && COTURNS_POSTINSTALL_STATUS_FILE="/tmp/${NAME}_postinstall_status.txt"
+
+[ -z "$SECONDARY_VNIC_NAME" ] && SECONDARY_VNIC_NAME="${ENVIRONMENT}-${ORACLE_REGION}-SecondaryVnic"
+[ -z "$INSTANCE_POOL_NAME" ] && INSTANCE_POOL_NAME="${ENVIRONMENT}-${ORACLE_REGION}-CoturnInstancePool"
+[ -z "$INSTANCE_CONFIG_NAME" ] && INSTANCE_CONFIG_NAME="${ENVIRONMENT}-${ORACLE_REGION}-CoturnInstanceConfig"
+[ -z "$AUTO_SCALING_CONFIG_NAME" ] && AUTO_SCALING_CONFIG_NAME="${ENVIRONMENT}-${ORACLE_REGION}-CoturnAutoScaleConfig"
+[ -z "$POLICY_NAME" ] && POLICY_NAME="${ENVIRONMENT}-${ORACLE_REGION}-CoturnScalingPolicy"
+[ -z "$SCALE_IN_RULE_NAME" ] && SCALE_IN_RULE_NAME="${ENVIRONMENT}-${ORACLE_REGION}-scalingLowLoad"
+[ -z "$SCALE_OUT_RULE_NAME" ] && SCALE_OUT_RULE_NAME="${ENVIRONMENT}-${ORACLE_REGION}-scalingHighLoad"
+
+[ -z "$S3_PROFILE" ] && S3_PROFILE="oracle"
+[ -z "$S3_STATE_BUCKET" ] && S3_STATE_BUCKET="tf-state-$ENVIRONMENT"
+[ -z "$S3_ENDPOINT" ] && S3_ENDPOINT="https://$ORACLE_S3_NAMESPACE.compat.objectstorage.$ORACLE_REGION.oraclecloud.com"
+[ -z "$S3_STATE_KEY" ] && S3_STATE_KEY="$ENVIRONMENT/coturns/$NAME/terraform.tfstate"
+
+TERRAFORM_MAJOR_VERSION=$(terraform -v | head -1  | awk '{print $2}' | cut -d'.' -f1)
+TF_GLOBALS_CHDIR=
+if [[ "$TERRAFORM_MAJOR_VERSION" == "v1" ]]; then
+  TF_GLOBALS_CHDIR="-chdir=$LOCAL_PATH"
+  TF_POST_PARAMS=
+else
+  TF_POST_PARAMS="$LOCAL_PATH"
+fi
+
+# The —reconfigure option disregards any existing configuration, preventing migration of any existing state
+terraform $TF_GLOBALS_CHDIR init \
+  -backend-config="bucket=$S3_STATE_BUCKET" \
+  -backend-config="key=$S3_STATE_KEY" \
+  -backend-config="region=$ORACLE_REGION" \
+  -backend-config="profile=$S3_PROFILE" \
+  -backend-config="endpoint=$S3_ENDPOINT" \
+  -reconfigure $TF_POST_PARAMS
+
+[ -z "$ACTION" ] && ACTION="apply"
+
+if [[ "$ACTION" == "apply" ]]; then
+  ACTION_POST_PARAMS="-auto-approve"
+fi
+if [[ "$ACTION" == "import" ]]; then
+  ACTION_POST_PARAMS="$1 $2"
+fi
+
+terraform $TF_GLOBALS_CHDIR $ACTION \
+  -var="tenancy_ocid=$TENANCY_OCID" \
+  -var="compartment_ocid=$COMPARTMENT_OCID" \
+  -var="oracle_region=$ORACLE_REGION" \
+  -var="user_public_key_path=$USER_PUBLIC_KEY_PATH" \
+  -var="user_private_key_path=$USER_PRIVATE_KEY_PATH" \
+  -var="bastion_host=$BASTION_HOST" \
+  -var="instance_config_name=$INSTANCE_CONFIG_NAME" \
+  -var="shape=$SHAPE" \
+  -var="ocpus=$OCPUS" \
+  -var="memory_in_gbs=$MEMORY_IN_GBS" \
+  -var="public_subnet_ocid=$PUBLIC_SUBNET_2_OCID" \
+  -var="private_subnet_ocid=$NAT_SUBNET_OCID" \
+  -var="secondary_vnic_name=$SECONDARY_VNIC_NAME" \
+  -var="image_ocid=$COTURN_IMAGE_OCID" \
+  -var="instance_pool_size=$DESIRED_CAPACITY" \
+  -var="instance_pool_name=$INSTANCE_POOL_NAME" \
+  -var="availability_domains=$AVAILABILITY_DOMAINS" \
+  -var="tag_namespace=$TAG_NAMESPACE" \
+  -var="environment=$ENVIRONMENT" \
+  -var="environment_type=$ENVIRONMENT_TYPE" \
+  -var="git_branch=$ORACLE_GIT_BRANCH" \
+  -var="domain=$DOMAIN" \
+  -var="name=$NAME" \
+  -var="shard_role=$SHARD_ROLE" \
+  -var="auto_scaling_config_name=$AUTO_SCALING_CONFIG_NAME" \
+  -var="scale_out_rule_name=$SCALE_OUT_RULE_NAME" \
+  -var="scale_in_rule_name=$SCALE_IN_RULE_NAME" \
+  -var="policy_name=$POLICY_NAME" \
+  -var="vcn_name=$VCN_NAME" \
+  -var="resource_name_root=$VCN_NAME_ROOT" \
+  -var="user=$SSH_USER" \
+  -var="coturns_postinstall_status_file=$COTURNS_POSTINSTALL_STATUS_FILE" \
+  -var "infra_configuration_repo=$INFRA_CONFIGURATION_REPO" \
+  -var "infra_customizations_repo=$INFRA_CUSTOMIZATIONS_REPO" \
+  $ACTION_POST_PARAMS $TF_POST_PARAMS
+
+if [[ "$ENVIRONMENT_TYPE" == "prod" ]]; then
+  echo "Tagging coturn image as production"
+  $LOCAL_PATH/../scripts/oracle_custom_images.py --tag_production --image_id $COTURN_IMAGE_OCID --region $ORACLE_REGION
+fi
---
+++ terraform/create-coturn-stack/create-coturn-stack-oracle.tf
@@ -0,0 +1,428 @@
+variable "tenancy_ocid" {}
+variable "compartment_ocid" {}
+variable "oracle_region" {}
+variable "instance_config_name" {}
+variable "shape" {}
+variable "memory_in_gbs" {}
+variable "ocpus" {}
+variable "public_subnet_ocid" {}
+variable "private_subnet_ocid" {}
+variable "secondary_vnic_name" {}
+variable "image_ocid" {}
+variable "instance_pool_size" {}
+variable "instance_pool_name" {}
+variable "availability_domains" {
+  type = list(string)
+}
+variable "tag_namespace" {}
+variable "environment" {}
+variable "environment_type" {}
+variable "git_branch" {}
+variable "domain" {}
+variable "shard_role" {}
+variable "name" {}
+variable "user_public_key_path" {}
+variable "user_private_key_path" {}
+variable "bastion_host" {}
+variable "auto_scaling_config_name" {}
+variable "scale_out_rule_name" {}
+variable "scale_in_rule_name" {}
+variable "policy_name" {}
+variable "vcn_name" {}
+variable "resource_name_root" {}
+variable "user" {}
+variable "coturns_postinstall_status_file" {}
+variable "user_data_file" {
+  default = "terraform/create-coturn-stack/user-data/postinstall-runner-oracle.sh"
+}
+variable "user_data_lib_path" {
+  default = "terraform/lib"
+}
+variable "infra_configuration_repo" {}
+variable "infra_customizations_repo" {}
+
+locals {
+  common_tags = {
+    "${var.tag_namespace}.environment" = var.environment
+    "${var.tag_namespace}.environment_type" = var.environment_type
+    "${var.tag_namespace}.git_branch" = var.git_branch
+    "${var.tag_namespace}.domain" = var.domain
+    "${var.tag_namespace}.Name" = var.name
+    "${var.tag_namespace}.shard-role" = var.shard_role
+  }
+}
+
+provider "oci" {
+  region = var.oracle_region
+  tenancy_ocid = var.tenancy_ocid
+}
+
+terraform {
+  backend "s3" {
+    skip_region_validation = true
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+    force_path_style = true
+  }
+  required_providers {
+      oci = {
+          source  = "oracle/oci"
+      }
+  }
+}
+
+// ============COTURN NETWORK SECURITY GROUP ============
+
+data "oci_core_vcns" "vcns" {
+  compartment_id = var.compartment_ocid
+  display_name = var.vcn_name
+}
+
+resource "oci_core_network_security_group" "coturn_network_security_group" {
+  compartment_id = var.compartment_ocid
+  vcn_id = data.oci_core_vcns.vcns.virtual_networks[0].id
+  display_name = "${var.resource_name_root}-CoturnSecurityGroup"
+}
+
+resource "oci_core_network_security_group_security_rule" "coturn_network_security_group_security_rule_1" {
+  network_security_group_id = oci_core_network_security_group.coturn_network_security_group.id
+  direction = "EGRESS"
+  destination = "0.0.0.0/0"
+  protocol = "all"
+}
+
+resource "oci_core_network_security_group_security_rule" "public_network_security_group_security_rule_2" {
+  network_security_group_id = oci_core_network_security_group.coturn_network_security_group.id
+  //tcp
+  protocol = "6"
+  direction = "INGRESS"
+  source = data.oci_core_vcns.vcns.virtual_networks[0].cidr_block
+  stateless = false
+
+  tcp_options {
+    destination_port_range {
+      min = 22
+      max = 22
+    }
+  }
+}
+
+resource "oci_core_network_security_group_security_rule" "coturn_network_security_group_security_rule_3" {
+  network_security_group_id = oci_core_network_security_group.coturn_network_security_group.id
+  //tcp
+  protocol = "6"
+  direction = "INGRESS"
+  source = "0.0.0.0/0"
+  stateless = false
+
+  tcp_options {
+    destination_port_range {
+      min = 443
+      max = 443
+    }
+  }
+}
+
+resource "oci_core_network_security_group_security_rule" "coturn_network_security_group_security_rule_4" {
+  network_security_group_id = oci_core_network_security_group.coturn_network_security_group.id
+  //udp
+  protocol = "17"
+  direction = "INGRESS"
+  source = "0.0.0.0/0"
+  stateless = false
+
+  udp_options {
+    destination_port_range {
+      min = 443
+      max = 443
+    }
+  }
+}
+
+// ============ COTURN INSTANCE POOL ============
+
+resource "oci_core_instance_configuration" "oci_instance_configuration" {
+  compartment_id = var.compartment_ocid
+  display_name = var.instance_config_name
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  defined_tags = local.common_tags
+
+  instance_details {
+    instance_type = "compute"
+
+    launch_details {
+      compartment_id = var.compartment_ocid
+      shape = var.shape
+
+      shape_config {
+        memory_in_gbs = var.memory_in_gbs
+        ocpus = var.ocpus
+      }
+
+      create_vnic_details {
+        subnet_id = var.public_subnet_ocid
+        nsg_ids = [
+          oci_core_network_security_group.coturn_network_security_group.id]
+        # disable auto-assignment of public IP for instance
+        assign_public_ip = false
+      }
+
+      source_details {
+        source_type = "image"
+        image_id = var.image_ocid
+      }
+
+      metadata = {
+        user_data = base64encode(join("",[
+          file("${path.cwd}/${var.user_data_lib_path}/postinstall-header.sh"), # load the header
+          file("${path.cwd}/${var.user_data_lib_path}/postinstall-lib.sh"), # load the lib
+          file("${path.cwd}/${var.user_data_lib_path}/postinstall-eip-lib.sh"), # load the EIP lib
+          "\nexport INFRA_CONFIGURATION_REPO=${var.infra_configuration_repo}\nexport INFRA_CUSTOMIZATIONS_REPO=${var.infra_customizations_repo}\n", #repo variables
+          file("${path.cwd}/${var.user_data_file}"), # load our customizations
+          file("${path.cwd}/${var.user_data_lib_path}/postinstall-footer.sh") # load the footer
+        ]))
+
+        ssh_authorized_keys = file(var.user_public_key_path)
+      }
+
+      defined_tags = local.common_tags
+      freeform_tags = {
+        configuration_repo = var.infra_configuration_repo
+        customizations_repo = var.infra_customizations_repo
+      }
+    }
+    secondary_vnics {
+      display_name = var.secondary_vnic_name
+
+      create_vnic_details {
+        assign_public_ip = false
+        display_name = var.secondary_vnic_name
+        subnet_id = var.private_subnet_ocid
+        nsg_ids = [
+          oci_core_network_security_group.coturn_network_security_group.id]
+
+        defined_tags = local.common_tags
+      }
+    }
+  }
+}
+
+resource "oci_core_instance_pool" "oci_instance_pool_1_ad" {
+  size = var.instance_pool_size
+  compartment_id = var.compartment_ocid
+  instance_configuration_id = oci_core_instance_configuration.oci_instance_configuration.id
+  display_name = var.instance_pool_name
+  count = length(var.availability_domains) == 1 ? 1:0
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  placement_configurations {
+    primary_subnet_id = var.public_subnet_ocid
+    secondary_vnic_subnets {
+      display_name = var.secondary_vnic_name
+      subnet_id = var.private_subnet_ocid
+    }
+    availability_domain = var.availability_domains[0]
+  }
+
+  defined_tags = local.common_tags
+}
+
+resource "oci_core_instance_pool" "oci_instance_pool_2_ad" {
+  size = var.instance_pool_size
+  compartment_id = var.compartment_ocid
+  instance_configuration_id = oci_core_instance_configuration.oci_instance_configuration.id
+  display_name = var.instance_pool_name
+  count = length(var.availability_domains) == 2 ? 1:0
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  placement_configurations {
+    primary_subnet_id = var.public_subnet_ocid
+    secondary_vnic_subnets {
+      display_name = var.secondary_vnic_name
+      subnet_id = var.private_subnet_ocid
+    }
+    availability_domain = var.availability_domains[0]
+  }
+
+  placement_configurations {
+    primary_subnet_id = var.public_subnet_ocid
+    secondary_vnic_subnets {
+      display_name = var.secondary_vnic_name
+      subnet_id = var.private_subnet_ocid
+    }
+    availability_domain = var.availability_domains[1 % length(var.availability_domains)]
+  }
+
+  defined_tags = local.common_tags
+}
+
+resource "oci_core_instance_pool" "oci_instance_pool_3_ad" {
+  size = var.instance_pool_size
+  compartment_id = var.compartment_ocid
+  instance_configuration_id = oci_core_instance_configuration.oci_instance_configuration.id
+  display_name = var.instance_pool_name
+  count = length(var.availability_domains) == 3 ? 1:0
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  placement_configurations {
+    primary_subnet_id = var.public_subnet_ocid
+    secondary_vnic_subnets {
+      display_name = var.secondary_vnic_name
+      subnet_id = var.private_subnet_ocid
+    }
+    availability_domain = var.availability_domains[0]
+  }
+  placement_configurations {
+    primary_subnet_id = var.public_subnet_ocid
+    secondary_vnic_subnets {
+      display_name = var.secondary_vnic_name
+      subnet_id = var.private_subnet_ocid
+    }
+    availability_domain = var.availability_domains[1 % length(var.availability_domains)]
+  }
+  placement_configurations {
+    primary_subnet_id = var.public_subnet_ocid
+    secondary_vnic_subnets {
+      display_name = var.secondary_vnic_name
+      subnet_id = var.private_subnet_ocid
+    }
+    availability_domain = var.availability_domains[2 % length(var.availability_domains)]
+  }
+
+  defined_tags = local.common_tags
+}
+
+resource "oci_autoscaling_auto_scaling_configuration" "oci_auto_scaling_configuration" {
+  compartment_id = var.compartment_ocid
+  is_enabled = "true"
+  display_name = var.auto_scaling_config_name
+
+  defined_tags = local.common_tags
+
+  policies {
+    capacity {
+      initial = var.instance_pool_size
+      max = var.instance_pool_size
+      min = var.instance_pool_size - 1
+    }
+
+    display_name = var.policy_name
+    policy_type = "threshold"
+
+    rules {
+      action {
+        type = "CHANGE_COUNT_BY"
+        value = "1"
+      }
+
+      display_name = var.scale_out_rule_name
+
+      metric {
+        metric_type = "CPU_UTILIZATION"
+
+        threshold {
+          operator = "GT"
+          value = "1"
+        }
+      }
+    }
+
+    rules {
+      action {
+        type = "CHANGE_COUNT_BY"
+        value = "-1"
+      }
+
+      display_name = var.scale_in_rule_name
+
+      metric {
+        metric_type = "CPU_UTILIZATION"
+
+        threshold {
+          operator = "LT"
+          value = "0"
+        }
+      }
+    }
+  }
+
+  auto_scaling_resources {
+    id = length(var.availability_domains) == 3 ? oci_core_instance_pool.oci_instance_pool_3_ad[0].id : (length(var.availability_domains) == 2 ? oci_core_instance_pool.oci_instance_pool_2_ad[0].id : oci_core_instance_pool.oci_instance_pool_1_ad[0].id)
+    type = "instancePool"
+  }
+}
+
+data "oci_core_instance_pool_instances" "oci_instance_pool_instances" {
+  count = 1
+  compartment_id = var.compartment_ocid
+  instance_pool_id = length(var.availability_domains) == 3 ? oci_core_instance_pool.oci_instance_pool_3_ad[0].id : (length(var.availability_domains) == 2 ? oci_core_instance_pool.oci_instance_pool_2_ad[0].id : oci_core_instance_pool.oci_instance_pool_1_ad[0].id)
+  depends_on = [
+    oci_core_instance_pool.oci_instance_pool_3_ad,
+    oci_core_instance_pool.oci_instance_pool_2_ad,
+    oci_core_instance_pool.oci_instance_pool_1_ad]
+}
+
+data "oci_core_instance" "oci_instance_datasources" {
+  count = var.instance_pool_size
+  instance_id = lookup(data.oci_core_instance_pool_instances.oci_instance_pool_instances[0].instances[count.index], "id")
+}
+
+resource "null_resource" "verify_cloud_init" {
+  count = var.instance_pool_size
+  depends_on = [
+    data.oci_core_instance.oci_instance_datasources]
+
+  connection {
+    type = "ssh"
+    host = element(data.oci_core_instance.oci_instance_datasources.*.private_ip, count.index)
+    user = var.user
+    private_key = file(var.user_private_key_path)
+
+    bastion_host = var.bastion_host
+    bastion_user = var.user
+    bastion_private_key = file(var.user_private_key_path)
+
+    timeout = "10m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cloud-init status --wait"
+    ]
+  }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+}
+
+resource "null_resource" "cloud_init_output" {
+  count = var.instance_pool_size
+  depends_on = [
+    null_resource.verify_cloud_init]
+
+  provisioner "local-exec" {
+    command = "ssh -o StrictHostKeyChecking=no -J ${var.user}@${var.bastion_host}  ${var.user}@${element(data.oci_core_instance.oci_instance_datasources.*.private_ip, count.index)} 'echo hostname: $HOSTNAME, privateIp: ${element(data.oci_core_instance.oci_instance_datasources.*.private_ip, count.index)} - $(cloud-init status)' >> ${var.coturns_postinstall_status_file}"
+  }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
+output "private_ips" {
+  value = [
+    data.oci_core_instance.oci_instance_datasources.*.private_ip]
+}
