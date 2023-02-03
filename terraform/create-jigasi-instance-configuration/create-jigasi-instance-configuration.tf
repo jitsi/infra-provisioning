28f3bd43fce108fdafa720320533e32d0e5138c9
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-02-03 07:01:25 -0600

    include terraform metadata

--- terraform/create-jigasi-instance-configuration/create-jigasi-instance-configuration.tf
+++ terraform/create-jigasi-instance-configuration/create-jigasi-instance-configuration.tf
@@ -34,6 +34,20 @@ provider "oci" {
   tenancy_ocid = var.tenancy_ocid
 }
 
+terraform {
+  backend "s3" {
+    skip_region_validation = true
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+    force_path_style = true
+  }
+  required_providers {
+      oci = {
+          source  = "oracle/oci"
+      }
+  }
+}
+
 locals {
   common_tags = {
     "${var.tag_namespace}.environment" = var.environment
