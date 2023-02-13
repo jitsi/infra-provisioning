764fdeaa7394b6b0445c1a4d28b118038c566151
scott boone <sawall@users.noreply.github.com>
2023-02-13 10:06:37 -0600

    recycle haproxy cleanly leaves consul (#60)

--- scripts/pool.py
+++ scripts/pool.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 ''' helper tool to interact with jitsi oracle instance pools'''
 
 import sys
@@ -15,13 +15,15 @@ def init_click_context(ctx: click.Context, environment: str, role: str, inactive
     '''
     initialize the context object
     \b
-    ctx.obj['OCI']          OCI clients per region
+    ctx.obj['OCI_COMP']     OCI Compute client per region (dict)
+    ctx.obj['OCI_MGMT']     OCI ComputeManagement clients per region (dict)
     ctx.obj['POOLS']        per region {} containing [{instance_pool/[instance]/role}]
     ctx.obj['ENVIRONMENT']  jitsi environment
     ctx.obj['FILTER']       pool filters
     '''
     ctx.ensure_object(dict)
-    ctx.obj['OCI'] = {}
+    ctx.obj['OCI_COMP'] = {}
+    ctx.obj['OCI_MGMT'] = {}
     ctx.obj['POOLS'] = {}
     ctx.obj['ENVIRONMENT'] = environment
     ctx.obj['FILTER'] = {}
@@ -33,9 +35,12 @@ def init_click_context(ctx: click.Context, environment: str, role: str, inactive
         click.echo("## DEBUG: create an oracle ComputeManagementClient for each region in OCI")
     oci_config = oci.config.from_file()
     for region in hcvlib.oracle_regions():
+        compute_client = oci.core.ComputeClient(oci_config)
+        compute_client.base_client.set_region(region)
+        ctx.obj['OCI_COMP'][region] = compute_client
         compute_management_client = oci.core.ComputeManagementClient(oci_config)
         compute_management_client.base_client.set_region(region)
-        ctx.obj['OCI'][region] = compute_management_client
+        ctx.obj['OCI_MGMT'][region] = compute_management_client
 
     if ctx.obj['DEBUG']:
         click.echo("## DEBUG: create a retry strategy for oracle calls to prevent fails during SCALING")
@@ -52,9 +57,9 @@ def load_instance_pools(ctx: click.Context):
     env_compartment = hcvlib.get_oracle_compartment_by_environment(ctx.obj['ENVIRONMENT'])
     ctx.obj['ALL_POOLS_RUNNING'] = True
 
-    for region in ctx.obj['OCI'].keys():
+    for region in ctx.obj['OCI_MGMT'].keys():
         filtered_region_instance_pools = []
-        compute_management_client = ctx.obj['OCI'][region]
+        compute_management_client = ctx.obj['OCI_MGMT'][region]
         region_pools = compute_management_client.list_instance_pools(env_compartment.id).data
         for pool in region_pools:
             if ctx.obj['DEBUG']:
@@ -127,7 +132,7 @@ def double_instance_pools(ctx: click.Context):
         for pool in ctx.obj['POOLS'][region]:
             click.echo(f"## doubling size of pool in {region}: {pool['instance_pool'].defined_tags['jitsi']['Name']}")
             instance_pool_details = oci.core.models.UpdateInstancePoolDetails(size=2*pool['instance_pool'].size)
-            compute_management_client = ctx.obj['OCI'][region]
+            compute_management_client = ctx.obj['OCI_MGMT'][region]
 
             updated_pool = compute_management_client.update_instance_pool(
                 instance_pool_id=pool['instance_pool'].id,
@@ -142,20 +147,22 @@ def double_instance_pools(ctx: click.Context):
 def halve_pool_instances_in_region(ctx: click.Context, region: str) -> Tuple[str, bool]:
     '''halve the size of instance pools in a region'''
     if len(ctx.obj['POOLS'][region]) < 1:
-        if ctx.obj['DEBUG']:
+        if ctx.obj['DEBUG'] and not ctx.obj['IP_ONLY']:
             click.echo(f"## DEBUG: skipping scale down in {region} (no instance pools)")
-    compute_management_client = ctx.obj['OCI'][region]
+    compute_management_client = ctx.obj['OCI_MGMT'][region]
     for pool in ctx.obj['POOLS'][region]:
         pool_name = pool['instance_pool'].defined_tags['jitsi']['Name']
         running_instances = [{'ocid': i.id, 'time_created': i.time_created, } for i in pool['instances'] if i.state == 'Running']
         if len(running_instances) <= int(ctx.obj['MINIMUM_POOL_SIZE']):
-            click.echo(f"## skipped halving pool in {region}: {pool_name} (pool has {len(running_instances)} " +
-                       f"running instances and minimum size is {int(ctx.obj['MINIMUM_POOL_SIZE'])})")
+            if not ctx.obj['IP_ONLY'] or ctx.obj['DEBUG']:
+                click.echo(f"## skipped halving pool in {region}: {pool_name} (pool has {len(running_instances)} " +
+                           f"running instances and minimum size is {int(ctx.obj['MINIMUM_POOL_SIZE'])})")
             continue
         floor_half_running = len(running_instances)//2 
         max_detachable = len(running_instances) - int(ctx.obj['MINIMUM_POOL_SIZE'])
         detach_int = min(floor_half_running, max_detachable) 
-        click.echo(f"## detaching {detach_int} of {len(running_instances)} instances from pool {pool_name}...")
+        if not ctx.obj['IP_ONLY']:
+            click.echo(f"## detaching {detach_int} of {len(running_instances)} instances from pool {pool_name}...")
         ordered_instances = sorted(pool['instances'], key=lambda x: x.time_created)
         for i in range(detach_int):
             instance_details = oci.core.models.DetachInstancePoolInstanceDetails(
@@ -163,22 +170,27 @@ def halve_pool_instances_in_region(ctx: click.Context, region: str) -> Tuple[str
                 is_auto_terminate=True,
                 is_decrement_size=True,
             )
-            click.echo(f"## {pool_name} [{i+1}/{detach_int}]: detaching {ordered_instances[i].id}")
-            try:
-                compute_management_client.detach_instance_pool_instance(
-                    instance_pool_id=pool['instance_pool'].id,
-                    detach_instance_pool_instance_details=instance_details,
-                    retry_strategy=ctx.obj['RETRY_STRATEGY'],
-                )
-            except oci.exceptions.TransientServiceError as transient_error:
-                click.echo(f"## {pool_name} [{i+1}/{detach_int}]: FAILED detach instance: {ordered_instances[i].id}:\n{transient_error}")
-                return(region, False)
-            click.echo(f"## {pool_name} [{i+1}/{detach_int}]: successfully detached instance")
+            if ctx.obj['IP_ONLY']:
+                instance = ctx.obj['OCI_COMP'][region].get_instance(instance_id=ordered_instances[i].id)
+                ctx.obj['IP_HITLIST'].append(instance.data.freeform_tags['private_ip'])
+            if not ctx.obj['IP_ONLY']:
+                click.echo(f"## {pool_name} [{i+1}/{detach_int}]: detaching {ordered_instances[i].id}")
+                try:
+                    compute_management_client.detach_instance_pool_instance(
+                        instance_pool_id=pool['instance_pool'].id,
+                        detach_instance_pool_instance_details=instance_details,
+                        retry_strategy=ctx.obj['RETRY_STRATEGY'],
+                    )
+                except oci.exceptions.TransientServiceError as transient_error:
+                    click.echo(f"## {pool_name} [{i+1}/{detach_int}]: FAILED detach instance: {ordered_instances[i].id}:\n{transient_error}")
+                    return(region, False)
+                click.echo(f"## {pool_name} [{i+1}/{detach_int}]: successfully detached instance")
     return (region, True)
 
 def halve_instance_pools(ctx: click.Context):
     '''halve the size of all instance pools in the context by detaching/deleting the oldest instances'''
-    click.echo(f"## pool.py: halving instance pools for role {ctx.obj['FILTER']['ROLE']} in {ctx.obj['ENVIRONMENT']}")
+    if not ctx.obj['IP_ONLY'] or ctx.obj['DEBUG']:
+        click.echo(f"## pool.py: halving instance pools for role {ctx.obj['FILTER']['ROLE']} in {ctx.obj['ENVIRONMENT']}")
     regions = ctx.obj['POOLS'].keys()
     with ThreadPoolExecutor(max_workers=len(regions)) as executor:
         future_results = { executor.submit(halve_pool_instances_in_region, ctx, region) for region in regions }
@@ -187,9 +199,12 @@ def halve_instance_pools(ctx: click.Context):
             if ctx.obj['DEBUG']:
                 click.echo(f"## DEBUG: half_pool_instances_in_region result: {data}")
             if not data[1]:
-                click.echo(f"## failed halve_instance_pools for at least one pool in region: {data[0]}")
+                if not ctx.obj['IP_ONLY'] or ctx.obj['DEBUG']:
+                    click.echo(f"## failed halve_instance_pools for at least one pool in region: {data[0]}")
     if ctx.obj['WAIT']:
         wait_for_running_pools(ctx)
+    if ctx.obj['IP_ONLY']:
+        click.echo(f"{' '.join(ctx.obj['IP_HITLIST'])}")
 
 @click.group(invoke_without_command=False, context_settings=dict(max_content_width=120))
 @click.option('--environment', required=True, envvar=['ENVIRONMENT', 'HCV_ENVIRONMENT'],
@@ -230,12 +245,15 @@ def double_cmd(ctx: click.Context, wait: bool):
 
 @cli.command('halve', short_help='scale down existing instance pools to half')
 @click.option('--minimum', envvar=['MINIMUM_POOL_SIZE'], default=1, help='minimum remaining pool size')
+@click.option('--onlyip', envvar=['IP_ONLY'], is_flag=True, default=False, help='no-op that only lists IPs that would be scaled down')
 @click.option('--wait', envvar=['WAIT'], is_flag=True, default=False, help='wait for pools to be in RUNNING state before exit')
 @click.pass_context
-def halve_cmd(ctx: click.Context, minimum: int, wait: bool):
+def halve_cmd(ctx: click.Context, minimum: int, onlyip: bool, wait: bool):
     '''halve the size of existing instance pools of a specified role'''
     ctx.obj['MINIMUM_POOL_SIZE'] = minimum
     ctx.obj['WAIT'] = wait
+    ctx.obj['IP_ONLY'] = onlyip
+    ctx.obj['IP_HITLIST'] = []
     if not ctx.obj['FILTER']['ROLE']:
         click.echo("## must set ROLE for scaling operations")
         sys.exit(1)
