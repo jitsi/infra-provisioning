52ee00b7ddc8460ace14139b0907945dcd6295d7
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-02-21 16:23:18 -0600

    check mount and fail if failed

--- scripts/update-ops-repo.sh
+++ scripts/update-ops-repo.sh
@@ -22,6 +22,11 @@ LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
 
 sudo /usr/bin/s3fs "$OPS_REPO_MOUNT_PATH" -o "bucket=$OPS_REPO_BUCKET" -o "passwd_file=$S3FS_PASSWORD_PATH" -o "url=https://$ORACLE_S3_NAMESPACE.compat.objectstorage.$ORACLE_REGION.oraclecloud.com" -o nomultipart -o use_path_request_style -o "endpoint=$ORACLE_REGION" -o allow_other -o umask=000
 
+if [ $? -gt 0 ]; then
+    echo "Failed to mount bucket, skipping further steps."
+    exit 5
+fi
+
 if [ -z "$UPSTREAM_CI_SERVER" ]; then
     echo "No upstream CI server specified, not copying any new files"
 else
