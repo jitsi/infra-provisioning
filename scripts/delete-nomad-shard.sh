2f9bd7b7dc8549177832b9c91895b6dca2b12e78
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-03-31 09:32:52 -0500

    also delete nomad shards

---
+++ scripts/delete-nomad-shard.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -x
+
+if [ -z "$ENVIRONMENT" ]; then
+    echo "No ENVIRONMENT set, exiting"
+    exit 2
+fi
+
+if [ -z "$SHARD" ]; then
+    echo "No SHARD set, exiting"
+    exit 2
+fi
+
+LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
+
+[ -e "$LOCAL_PATH/../sites/$ENVIRONMENT/stack-env.sh" ] && . "$LOCAL_PATH/../sites/$ENVIRONMENT/stack-env.sh"
+
+if [ -z "$ORACLE_REGION" ]; then
+  # Extract EC2_REGION from the shard name and use it to get the ORACLE_REGION
+  ORACLE_REGION=$($LOCAL_PATH/../../scripts/shard.py --shard_region --environment=$ENVIRONMENT --shard=$SHARD)
+fi
+
+if [ -z "$ORACLE_REGION" ]; then
+    echo "No ORACLE_REGION set, exiting"
+    exit 2
+fi
+
+[ -z "$LOCAL_REGION" ] && LOCAL_REGION="$ORACLE_REGION"
+
+if [ -z "$NOMAD_ADDR" ]; then
+    NOMAD_IPS="$(DATACENTER="$ENVIRONMENT-$LOCAL_REGION" OCI_DATACENTERS="$ENVIRONMENT-$LOCAL_REGION" ENVIRONMENT="$ENVIRONMENT" FILTER_ENVIRONMENT="false" SHARD='' RELEASE_NUMBER='' SERVICE="nomad-servers" DISPLAY="addresses" $LOCAL_PATH/consul-search.sh ubuntu)"
+    if [ -n "$NOMAD_IPS" ]; then
+        NOMAD_IP="$(echo $NOMAD_IPS | cut -d ' ' -f1)"
+        export NOMAD_ADDR="http://$NOMAD_IP:4646"
+    else
+        echo "No NOMAD_IPS for in environment $ENVIRONMENT in consul"
+        exit 5
+    fi
+fi
+
+if [ -z "$NOMAD_ADDR" ]; then
+    echo "Failed to set NOMAD_ADDR, exiting"
+    exit 5
+fi
+
+nomad job stop $SHARD
+exit $?
