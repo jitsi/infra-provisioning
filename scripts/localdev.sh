6eae3e29fe747225342e474f1f0972fc5005e81a
scott boone <sawall@users.noreply.github.com>
2023-04-03 15:34:28 -0500

    script to create symlinks for local dev (#99)

---
+++ scripts/localdev.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# creates symlinks for local script development from infra-provisioning
+#
+# assumes that infra-provisioning, infra-configuration, and a customization
+# repo named $CUSTOMIZATION_DIRNAME exist as directories in the same directory
+
+if [ -z "$CUSTOMIZATION_DIRNAME" ];then
+    echo "## no CUSTOMIZATION_DIRNAME found, exiting..."
+    exit 1
+fi
+
+LOCAL_PATH=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+cd $LOCAL_PATH/..
+
+echo "## creating symlinks needed for local script development"
+
+ln -s ../infra-configuration/ansible
+ln -s ../${CUSTOMIZATION_DIRNAME}/cloud_vpcs
+ln -s ../${CUSTOMIZATION_DIRNAME}/clouds
+ln -s ../${CUSTOMIZATION_DIRNAME}/config
+ln -s ../${CUSTOMIZATION_DIRNAME}/regions
+ln -s ../${CUSTOMIZATION_DIRNAME}/sites
+
+cd ../infra-configuration
+ln -s ../${CUSTOMIZATION_DIRNAME}/config
+ln -s ../${CUSTOMIZATION_DIRNAME}/sites
+
+cd ansible
+ln -s ../${CUSTOMIZATION_DIRNAME}/config
+ln -s ../${CUSTOMIZATION_DIRNAME}/secrets
+ln -s ../${CUSTOMIZATION_DIRNAME}/sites
+
+cd ../../${CUSTOMIZATION_DIRNAME}/ansible
+ln -s ../config
+ln -s ../sites
+
