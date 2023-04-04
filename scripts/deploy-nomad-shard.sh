88ff0948c5428a6e5bf3b5902e2a05fcc9b7ce72
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-04-04 09:11:02 -0500

    task: further nomad improvements

--- scripts/deploy-nomad-shard.sh
+++ scripts/deploy-nomad-shard.sh
@@ -44,12 +44,47 @@ fi
 
 [ -z "$DOCKER_TAG" ] && DOCKER_TAG="unstable-$(date +%Y-%m-%d)"
 
+if [ -n "$SIGNAL_VERSION" ]; then
+    if [ -z "$JICOFO_VERSION" ]; then
+        JICOFO_VERSION="$(echo $SIGNAL_VERSION | cut -d'-' -f1)"
+    fi
+    if [ -z "$JITSI_MEET_VERSION" ]; then
+        JITSI_MEET_VERSION="$(echo $SIGNAL_VERSION | cut -d'-' -f2)"
+    fi
+    if [ -z "$PROSODY_VERSION" ]; then
+        PROSODY_VERSION="$(echo $SIGNAL_VERSION | cut -d'-' -f3)"
+    fi
+else
+    if [ -n "$JICOFO_VERSION" ]; then
+        SIGNAL_VERSION="$JICOFO_VERSION-$JITSI_MEET_VERSION-$PROSODY_VERSION"
+    else
+        SIGNAL_VERSION="$DOCKER_TAG"
+    fi
+fi
+
+if [ -n "$JICOFO_VERSION" ]; then
+    JICOFO_TAG="jicofo-1.0-$JICOFO_VERSION-1"
+fi
+
+if [ -n "$JITSI_MEET_VERSION" ]; then
+    WEB_TAG="web-1.0.$JITSI_MEET_VERSION-1"
+fi
+
+if [ -n "$PROSODY_VERSION" ]; then
+    PROSODY_TAG="prosody-$PROSODY_VERSION"
+fi
+
+[ -z "$JICOFO_TAG" ] && JICOFO_TAG="$DOCKER_TAG"
+[ -z "$WEB_TAG" ] && WEB_TAG="$DOCKER_TAG"
+[ -z "$PROSODY_TAG" ] && PROSODY_TAG="$DOCKER_TAG"
+
 NOMAD_JOB_PATH="$LOCAL_PATH/../nomad"
 NOMAD_DC="$ENVIRONMENT-$ORACLE_REGION"
 
 [ -z "$ENCRYPTED_JVB_CREDENTIALS_FILE" ] && ENCRYPTED_JVB_CREDENTIALS_FILE="$LOCAL_PATH/../ansible/secrets/jvb.yml"
 [ -z "$ENCRYPTED_JIBRI_CREDENTIALS_FILE" ] && ENCRYPTED_JIBRI_CREDENTIALS_FILE="$LOCAL_PATH/../ansible/secrets/jibri.yml"
 [ -z "$ENCRYPTED_JIGASI_CREDENTIALS_FILE" ] && ENCRYPTED_JIGASI_CREDENTIALS_FILE="$LOCAL_PATH/../ansible/secrets/jigasi.yml"
+[ -z "$ENCRYPTED_COTURN_CREDENTIALS_FILE" ] && ENCRYPTED_COTURN_CREDENTIALS_FILE="$LOCAL_PATH/../ansible/secrets/coturn.yml"
 [ -z "$ENVIRONMENT_CONFIGURATION_FILE" ] && ENVIRONMENT_CONFIGURATION_FILE="$LOCAL_PATH/../sites/$ENVIRONMENT/vars.yml"
 [ -z "$MAIN_CONFIGURATION_FILE" ] && MAIN_CONFIGURATION_FILE="$LOCAL_PATH/../config/vars.yml"
 
@@ -63,6 +98,8 @@ JWT_ASAP_KEYSERVER_VARIABLE="prosody_public_key_repo_url"
 JWT_ACCEPTED_ISSUERS_VARIABLE="prosody_asap_accepted_issuers"
 JWT_ACCEPTED_AUDIENCES_VARIABLE="prosody_asap_accepted_audiences"
 TURNRELAY_HOST_VARIABLE="prosody_mod_turncredentials_hosts"
+TURNRELAY_PASSWORD_VARIABLE="coturn_secret"
+ENABLE_MUC_ALLOWNERS_VARIABLE="prosody_muc_allowners"
 
 # ensure no output for ansible vault contents and fail if ansible-vault fails
 set +x
@@ -72,6 +109,7 @@ export NOMAD_VAR_jvb_auth_password="$(ansible-vault view $ENCRYPTED_JVB_CREDENTI
 export NOMAD_VAR_jibri_xmpp_password="$(ansible-vault view $ENCRYPTED_JIBRI_CREDENTIALS_FILE --vault-password $VAULT_PASSWORD_FILE | yq eval ".${JIBRI_XMPP_PASSWORD_VARIABLE}" -)"
 export NOMAD_VAR_jibri_recorder_password="$(ansible-vault view $ENCRYPTED_JIBRI_CREDENTIALS_FILE --vault-password $VAULT_PASSWORD_FILE | yq eval ".${JIBRI_RECORDER_PASSWORD_VARIABLE}" -)"
 export NOMAD_VAR_jigasi_xmpp_password="$(ansible-vault view $ENCRYPTED_JIGASI_CREDENTIALS_FILE --vault-password $VAULT_PASSWORD_FILE | yq eval ".${JIGASI_XMPP_PASSWORD_VARIABLE}" -)"
+export NOMAD_VAR_turnrelay_password="$(ansible-vault view $ENCRYPTED_COTURN_CREDENTIALS_FILE --vault-password $VAULT_PASSWORD_FILE | yq eval ".${TURNRELAY_PASSWORD_VARIABLE}" -)"
 
 export NOMAD_VAR_jicofo_auth_password="$(cat $ENVIRONMENT_CONFIGURATION_FILE | yq eval .${JICOFO_XMPP_PASSWORD_VARIABLE} -)"
 export NOMAD_VAR_jwt_asap_keyserver="$(cat $ENVIRONMENT_CONFIGURATION_FILE | yq eval .${JWT_ASAP_KEYSERVER_VARIABLE} -)"
@@ -86,8 +124,12 @@ if [[ "$TURNRELAY_HOST_ARRAY" != "null" ]]; then
     export NOMAD_VAR_turnrelay_host="$(echo $TURNRELAY_HOST_ARRAY | yq eval '.[0]' -)"
 fi
 
-export NOMAD_VAR_jwt_accepted_issuers="$(cat $ENVIRONMENT_CONFIGURATION_FILE | yq eval ".${JWT_ACCEPTED_ISSUERS_VARIABLE} | @csv" -)"
-export NOMAD_VAR_jwt_accepted_audiences="$(cat $ENVIRONMENT_CONFIGURATION_FILE | yq eval ".${JWT_ACCEPTED_AUDIENCES_VARIABLE} | @csv" -)"
+export NOMAD_VAR_jwt_accepted_issuers="$(cat $MAIN_CONFIGURATION_FILE | yq eval ".${JWT_ACCEPTED_ISSUERS_VARIABLE} | @csv" -)"
+export NOMAD_VAR_jwt_accepted_audiences="$(cat $MAIN_CONFIGURATION_FILE | yq eval ".${JWT_ACCEPTED_AUDIENCES_VARIABLE} | @csv" -)"
+export ENABLE_MUC_ALLOWNERS="$(cat $ENVIRONMENT_CONFIGURATION_FILE | yq eval .${ENABLE_MUC_ALLOWNERS_VARIABLE} -)"
+if [[ "$ENABLE_MUC_ALLOWNERS" != "null" ]]; then
+    export NOMAD_VAR_enable_muc_allowners="$ENABLE_MUC_ALLOWNERS"
+fi
 
 export NOMAD_VAR_environment="$ENVIRONMENT"
 export NOMAD_VAR_domain="$DOMAIN"
@@ -95,7 +137,10 @@ export NOMAD_VAR_shard="$SHARD"
 export NOMAD_VAR_octo_region="$ORACLE_REGION"
 # [ -n "$SHARD_STATE" ] && export NOMAD_VAR_shard_state="$SHARD_STATE"
 export NOMAD_VAR_release_number="$RELEASE_NUMBER"
-export NOMAD_VAR_tag="$DOCKER_TAG"
+export NOMAD_VAR_signal_version="$SIGNAL_VERSION"
+export NOMAD_VAR_jicofo_tag="$JICOFO_TAG"
+export NOMAD_VAR_web_tag="$WEB_TAG"
+export NOMAD_VAR_prosody_tag="$PROSODY_TAG"
 export NOMAD_VAR_pool_type="$NOMAD_POOL_TYPE"
 
 sed -e "s/\[JOB_NAME\]/$SHARD/" "$NOMAD_JOB_PATH/shard.hcl" | nomad job run -var="dc=$NOMAD_DC" -
