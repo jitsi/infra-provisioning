c40069e1a97c425bb625b144ed2e0d4ba6047e63
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-05-17 15:14:51 -0500

    wait for load balancer to go healthy before detaching nomad instances

--- scripts/rotate-nomad-pool-oracle.sh
+++ scripts/rotate-nomad-pool-oracle.sh
@@ -14,6 +14,13 @@ fi
 [ -e "$LOCAL_PATH/../clouds/all.sh" ] && . $LOCAL_PATH/../clouds/all.sh
 [ -e "$LOCAL_PATH/../clouds/oracle.sh" ] && . $LOCAL_PATH/../clouds/oracle.sh
 
+[ -z "$MAX_WAIT_SECONDS" ] && MAX_WAIT_SECONDS=1200 # 20 min
+[ -z "$WAIT_INTERVAL_SECONDS" ] && WAIT_INTERVAL_SECONDS=30
+
+# if no load balancer is available to detect bootup health then
+# wait a fixed period between instances when rotating
+[ -z "$STARTUP_GRACE_PERIOD_SECONDS" ] && STARTUP_GRACE_PERIOD_SECONDS=300 # 5 min
+
 if [ -z "$ORACLE_REGION" ]; then
   echo "No ORACLE_REGION found.  Exiting..."
   exit 203
@@ -49,6 +56,24 @@ else
   INSTANCE_POOL_ID=$(echo "$INSTANCE_POOL_DETAILS" | jq -r '.id')
   export INSTANCE_POOL_SIZE=$(echo "$INSTANCE_POOL_DETAILS" | jq -r '.size')
 
+  EXISTING_INSTANCE_DATA=$(oci compute-management instance-pool list-instances --compartment-id "$COMPARTMENT_OCID" --instance-pool-id "$INSTANCE_POOL_ID" --region "$ORACLE_REGION" --all)
+  EXISTING_INSTANCES="$(echo "$EXISTING_INSTANCE_DATA" | jq .data)"
+  INSTANCE_COUNT=$(echo $EXISTING_INSTANCES| jq -r ".|length")
+
+  if [[ $INSTANCE_COUNT -gt 0 ]]; then
+    # more than local region found, check/perform association
+    for i in `seq 0 $((INSTANCE_COUNT-1))`; do
+      DETAILS="$(echo "$EXISTING_INSTANCES" | jq ".[$i]")"
+      INSTANCE_ID="$(echo "$DETAILS"  | jq -r ".id")"
+
+      # look up current load balancer, use if defined
+      LOAD_BALANCER_ID=$(echo "$DETAILS"  | jq -r '."load-balancer-backends"|first|."load-balancer-id"')
+      [[ "$LOAD_BALANCER_ID" == "null" ]] && LOAD_BALANCER_ID=
+      LB_BACKEND_SET_NAME=$(echo "$DETAILS"  | jq -r '."load-balancer-backends"|first|."backend-set-name"')
+      [[ "$LB_BACKEND_SET_NAME" == "null" ]] && LB_BACKEND_SET_NAME=
+    done
+  fi
+
   # first apply changes to instance configuration, etc
   $LOCAL_PATH/../terraform/nomad-pool/create-nomad-pool-stack.sh
 
@@ -63,6 +88,32 @@ else
   # next scale up by 2X
   echo -e "\n## rotate-nomad-poool-oracle: double the size of nomad pool"
   ENVIRONMENT=$ENVIRONMENT ROLE=nomad-pool INSTANCE_POOL_ID=$INSTANCE_POOL_ID ORACLE_REGIONS=$ORACLE_REGION $LOCAL_PATH/pool.py double --wait
+
+  # wait for load balancer to see new instances go healthy
+  if [ ! -z "$LOAD_BALANCER_ID" ]; then
+    # Wait for the LB to see all the backends (including the newly added instance) healthy to avoid downtime
+    WAIT_TOTAL=0
+    LATEST_LB_BACKEND_HEALTH=$(oci lb backend-set-health get --region "$ORACLE_REGION" --backend-set-name "$LB_BACKEND_SET_NAME" --load-balancer-id "$LOAD_BALANCER_ID")
+    LATEST_LB_BACKEND_OVERALL_STATUS=$(echo $LATEST_LB_BACKEND_HEALTH | jq -r '.data.status')
+    while [ "$LATEST_LB_BACKEND_OVERALL_STATUS" != 'OK' ]; do
+      if [ $WAIT_TOTAL -gt $MAX_WAIT_SECONDS ]; then
+        echo "Exceeding max waiting time of $MAX_WAIT_SECONDS seconds for the load balancer backend state to reach OK status again, current status is $LATEST_LB_BACKEND_OVERALL_STATUS. Something is wrong, exiting..."
+        exit 224
+      fi
+
+      echo "Waiting for the load balancer backend state to reach OK status again. Current status is $LATEST_LB_BACKEND_OVERALL_STATUS."
+      sleep $WAIT_INTERVAL_SECONDS
+      WAIT_TOTAL=$((WAIT_TOTAL + WAIT_INTERVAL_SECONDS))
+      LATEST_LB_BACKEND_HEALTH=$(oci lb backend-set-health get --region "$ORACLE_REGION" --backend-set-name "$LB_BACKEND_SET_NAME" --load-balancer-id "$LOAD_BALANCER_ID")
+      LATEST_LB_BACKEND_OVERALL_STATUS=$(echo $LATEST_LB_BACKEND_HEALTH | jq -r '.data.status')
+    done
+  else
+    # No load balancer to detect healthy state, so wait for fixed duration before continuing
+    if [[ $i -lt $((INSTANCE_COUNT-1)) ]]; then
+      echo "Waiting for $STARTUP_GRACE_PERIOD_SECONDS seconds before rotating next instance"
+      sleep $STARTUP_GRACE_PERIOD_SECONDS
+    fi
+  fi
 
   DETACHABLE_IPS=$(ENVIRONMENT=$ENVIRONMENT MINIMUM_POOL_SIZE=2 ROLE=nomad-pool INSTANCE_POOL_ID=$INSTANCE_POOL_ID ORACLE_REGIONS=$ORACLE_REGION $LOCAL_PATH/pool.py halve --onlyip)
 
