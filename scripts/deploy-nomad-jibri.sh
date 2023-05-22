197a344abd6d632bdc7bc95fdc5f0075ff86ff68
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-05-22 14:05:29 -0500

    feature: first pass at working jibri in nomad

---
+++ scripts/deploy-nomad-jibri.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -x
+
+if [ -z "$ENVIRONMENT" ]; then
+    echo "No ENVIRONMENT set, exiting"
+    exit 2
+fi
+
+LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
+
+[ -e "$LOCAL_PATH/../sites/$ENVIRONMENT/stack-env.sh" ] && . "$LOCAL_PATH/../sites/$ENVIRONMENT/stack-env.sh"
+[ -e "$LOCAL_PATH/../clouds/all.sh" ] && . "$LOCAL_PATH/../clouds/all.sh"
+
+[ -z "$VAULT_PASSWORD_FILE" ] && VAULT_PASSWORD_FILE="$LOCAL_PATH/../.vault-password.txt"
+
+if [ -z "$ORACLE_REGION" ]; then
+    echo "No ORACLE_REGION set, exiting"
+    exit 2
+fi
+
+[ -z "$LOCAL_REGION" ] && LOCAL_REGION="us-phoenix-1"
+
+if [ -z "$NOMAD_ADDR" ]; then
+    NOMAD_IPS="$(DATACENTER="$ENVIRONMENT-$LOCAL_REGION" OCI_DATACENTERS="$ENVIRONMENT-$LOCAL_REGION" ENVIRONMENT="$ENVIRONMENT" FILTER_ENVIRONMENT="false" SHARD='' RELEASE_NUMBER='' SERVICE="nomad-servers" DISPLAY="addresses" $LOCAL_PATH/consul-search.sh ubuntu)"
+    if [ -n "$NOMAD_IPS" ]; then
+        NOMAD_IP="$(echo $NOMAD_IPS | cut -d ' ' -f1)"
+        export NOMAD_ADDR="http://$NOMAD_IP:4646"
+    else
+        echo "No NOMAD_IPS for in environment $ENVIRONMENT in consul"
+        exit 5
+    fi
+fi
+
+if [ -z "$NOMAD_ADDR" ]; then
+    echo "Failed to set NOMAD_ADDR, exiting"
+    exit 5
+fi
+
+[ -z "$NOMAD_POOL_TYPE" ] && NOMAD_POOL_TYPE="general"
+
+[ -z "$DOCKER_TAG" ] && DOCKER_TAG="unstable-$(date +%Y-%m-%d)"
+
+if [ -n "$JIBRI_VERSION" ]; then
+    JIBRI_TAG="jibri-1.0-$JIBRI_VERSION-1"
+fi
+
+[ -z "$JIBRI_TAG" ] && JIBRI_TAG="$DOCKER_TAG"
+
+NOMAD_JOB_PATH="$LOCAL_PATH/../nomad"
+NOMAD_DC="$ENVIRONMENT-$ORACLE_REGION"
+
+[ -z "$ENCRYPTED_JIBRI_CREDENTIALS_FILE" ] && ENCRYPTED_JIBRI_CREDENTIALS_FILE="$LOCAL_PATH/../ansible/secrets/jibri.yml"
+[ -z "$ENVIRONMENT_CONFIGURATION_FILE" ] && ENVIRONMENT_CONFIGURATION_FILE="$LOCAL_PATH/../sites/$ENVIRONMENT/vars.yml"
+[ -z "$MAIN_CONFIGURATION_FILE" ] && MAIN_CONFIGURATION_FILE="$LOCAL_PATH/../config/vars.yml"
+
+JIBRI_XMPP_PASSWORD_VARIABLE="jibri_auth_password"
+JIBRI_RECORDER_PASSWORD_VARIABLE="jibri_selenium_auth_password"
+
+# ensure no output for ansible vault contents and fail if ansible-vault fails
+set +x
+set -e
+set -o pipefail
+export NOMAD_VAR_jibri_xmpp_password="$(ansible-vault view $ENCRYPTED_JIBRI_CREDENTIALS_FILE --vault-password $VAULT_PASSWORD_FILE | yq eval ".${JIBRI_XMPP_PASSWORD_VARIABLE}" -)"
+export NOMAD_VAR_jibri_recorder_password="$(ansible-vault view $ENCRYPTED_JIBRI_CREDENTIALS_FILE --vault-password $VAULT_PASSWORD_FILE | yq eval ".${JIBRI_RECORDER_PASSWORD_VARIABLE}" -)"
+
+set -x
+
+export NOMAD_VAR_environment="$ENVIRONMENT"
+export NOMAD_VAR_domain="$DOMAIN"
+# [ -n "$SHARD_STATE" ] && export NOMAD_VAR_shard_state="$SHARD_STATE"
+export NOMAD_VAR_jibri_tag="$JIBRI_TAG"
+export NOMAD_VAR_pool_type="$NOMAD_POOL_TYPE"
+
+sed -e "s/\[JOB_NAME\]/jibri-${ORACLE_REGION}/" "$NOMAD_JOB_PATH/jibri.hcl" | nomad job run -var="dc=$NOMAD_DC" -
