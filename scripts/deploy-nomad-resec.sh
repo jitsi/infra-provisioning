fc324677dbaa89c08bf4b321a9fc297e77d89cca
Aaron van Meerten <aaron.van.meerten@gmail.com>
2023-06-01 11:38:04 -0500

    feature: redis resec in nomad (#165)

---
+++ scripts/deploy-nomad-resec.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ -z "$ENVIRONMENT" ]; then
+    echo "No ENVIRONMENT set, exiting"
+    exit 2
+fi
+
+LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
+
+[ -e "$LOCAL_PATH/../sites/$ENVIRONMENT/stack-env.sh" ] && . "$LOCAL_PATH/../sites/$ENVIRONMENT/stack-env.sh"
+
+[ -e "$LOCAL_PATH/../clouds/all.sh" ] && . "$LOCAL_PATH/../clouds/all.sh"
+
+if [ -z "$ORACLE_REGION" ]; then
+    echo "No ORACLE_REGION set, exiting"
+    exit 2
+fi
+
+[ -z "$LOCAL_REGION" ] && LOCAL_REGION="us-phoenix-1"
+
+if [ -z "$NOMAD_ADDR" ]; then
+    NOMAD_IPS="$(DATACENTER="$ENVIRONMENT-$LOCAL_REGION" OCI_DATACENTERS="$ENVIRONMENT-$LOCAL_REGION" ENVIRONMENT="$ENVIRONMENT" FILTER_ENVIRONMENT="false" SHARD='' RELEASE_NUMBER='' SERVICE="nomad-servers" DISPLAY="addresses" $LOCAL_PATH/consul-search.sh ubuntu)"
+    if [ -n "$NOMAD_IPS" ]; then
+        NOMAD_IP="$(echo $NOMAD_IPS | cut -d ' ' -f1)"
+        export NOMAD_ADDR="http://$NOMAD_IP:4646"
+    else
+        echo "No NOMAD_IPS for in environment $ENVIRONMENT in consul"
+        exit 5
+    fi
+fi
+
+if [ -z "$NOMAD_ADDR" ]; then
+    echo "Failed to set NOMAD_ADDR, exiting"
+    exit 5
+fi
+
+NOMAD_JOB_PATH="$LOCAL_PATH/../nomad"
+NOMAD_DC="$ENVIRONMENT-$ORACLE_REGION"
+JOB_NAME="resec-$ORACLE_REGION"
+
+sed -e "s/\[JOB_NAME\]/$JOB_NAME/" "$NOMAD_JOB_PATH/resec.hcl" | nomad job run -var="dc=$NOMAD_DC" -
