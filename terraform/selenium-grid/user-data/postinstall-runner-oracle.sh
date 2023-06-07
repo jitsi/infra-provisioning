e81bcbf761723ee72c127fc18d993011ea3a90b4
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-06-07 10:48:47 -0500

    task: selenium grid unique hostname

--- terraform/selenium-grid/user-data/postinstall-runner-oracle.sh
+++ terraform/selenium-grid/user-data/postinstall-runner-oracle.sh
@@ -12,6 +12,9 @@ export GRID_ROLE=$(cat $CACHE_PATH | jq -r --arg GRID_ROLE_TAG "$GRID_ROLE_TAG" 
 export GRID=$(cat $CACHE_PATH | jq -r --arg GRID_TAG "$GRID_TAG" ".[\"$GRID_TAG\"]")
 
 export HOST_ROLE="$GRID-grid"
-export MY_HOSTNAME="${CLOUD_NAME}-${HOST_ROLE}.oracle.infra.jitsi.net"
+MY_IP=`curl -s curl http://169.254.169.254/opc/v1/vnics/ | jq .[0].privateIp -r`
+export MY_COMPONENT_NUMBER="$(echo $MY_IP | awk -F. '{print $2"-"$3"-"$4}')"
+
+export MY_HOSTNAME="${ORACLE_REGION}-${HOST_ROLE}-$MY_COMPONENT_NUMBER.oracle.infra.jitsi.net"
 export ANSIBLE_VARS="hcv_environment=$ENVIRONMENT cloud_name=$CLOUD_NAME selenium_grid_name=$GRID selenium_grid_role=$GRID_ROLE cloud_provider=oracle region=$ORACLE_REGION oracle_region=$ORACLE_REGION"
 
