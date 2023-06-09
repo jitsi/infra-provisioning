c94e21982d60898d918022bdcab8e6bdb6fb1ac5
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-06-09 14:42:18 -0500

    remove nomad redeploy from post-attach, was causing restarts

--- scripts/rotate-coturn-post-attach.sh
+++ scripts/rotate-coturn-post-attach.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
-LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
-
-# re-run coturn nomad deploy for region to ensure containers are up on new nodes
-if [[ "$NOMAD_COTURN_FLAG" == "true" ]]; then
-    $LOCAL_PATH/deploy-nomad-coturn.sh
-fi
-
 GOOD_PORT=443
 WAIT_GOOD=120
 # assumes pre attach ran
