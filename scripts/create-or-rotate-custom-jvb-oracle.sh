3885029969a07ce7fed5b2133dcce8aa46a52b34
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-08-17 13:00:57 -0400

    fix A1 OCPU count

--- scripts/create-or-rotate-custom-jvb-oracle.sh
+++ scripts/create-or-rotate-custom-jvb-oracle.sh
@@ -109,6 +109,11 @@ if [[ "$SHAPE" == "VM.Standard.E4.Flex" ]]; then
   [ -z "$MEMORY_IN_GBS" ] && MEMORY_IN_GBS=16
 fi
 
+if [[ "$SHAPE" == "VM.Standard.A1.Flex" ]]; then
+  [ -z "$OCPUS" ] && OCPUS=8
+  [ -z "$MEMORY_IN_GBS" ] && MEMORY_IN_GBS=16
+fi
+
 if [[ "$SHAPE" == "VM.Standard.E3.Flex" ]]; then
   [ -z "$OCPUS" ] && OCPUS=4
   [ -z "$MEMORY_IN_GBS" ] && MEMORY_IN_GBS=16
