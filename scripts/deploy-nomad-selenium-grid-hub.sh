aae1c278bdcffd22e910d66a1ba79f1c00f4b5a6
Aaron van Meerten <aaron.van.meerten@gmail.com>
2023-08-18 14:38:57 -0400

    task: first pass at selenium grid in nomad (#216)

---
+++ scripts/deploy-nomad-selenium-grid-hub.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ -z "$ENVIRONMENT" ]; then
+    echo "No ENVIRONMENT set, exiting"
+    exit 2
+fi
+
+LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
+
+[ -e "$LOCAL_PATH/../sites/$ENVIRONMENT/stack-env.sh" ] && . "$LOCAL_PATH/../sites/$ENVIRONMENT/stack-env.sh"
+
+[ -e "$LOCAL_PATH/../clouds/all.sh" ] && . "$LOCAL_PATH/../clouds/all.sh"
+
+if [ -z "$ORACLE_REGION" ]; then
+    echo "No ORACLE_REGION set, exiting"
+    exit 2
+fi
+
+if [ -z "$GRID" ]; then
+    echo "No GRID set, exiting"
+    exit 2
+fi
+
+[ -z "$LOCAL_REGION" ] && LOCAL_REGION="us-phoenix-1"
+
+if [ -z "$NOMAD_ADDR" ]; then
+    NOMAD_IPS="$(DATACENTER="$ENVIRONMENT-$LOCAL_REGION" OCI_DATACENTERS="$ENVIRONMENT-$LOCAL_REGION" ENVIRONMENT="$ENVIRONMENT" FILTER_ENVIRONMENT="false" SHARD='' RELEASE_NUMBER='' SERVICE="nomad-servers" DISPLAY="addresses" $LOCAL_PATH/consul-search.sh ubuntu)"
+    if [ -n "$NOMAD_IPS" ]; then
+        NOMAD_IP="$(echo $NOMAD_IPS | cut -d ' ' -f1)"
+        export NOMAD_ADDR="http://$NOMAD_IP:4646"
+    else
+        echo "No NOMAD_IPS for in environment $ENVIRONMENT in consul"
+        exit 5
+    fi
+fi
+
+if [ -z "$NOMAD_ADDR" ]; then
+    echo "Failed to set NOMAD_ADDR, exiting"
+    exit 5
+fi
+
+NOMAD_JOB_PATH="$LOCAL_PATH/../nomad"
+NOMAD_DC="$ENVIRONMENT-$ORACLE_REGION"
+JOB_NAME="grid-hub-$GRID"
+export NOMAD_VAR_grid="$GRID"
+
+sed -e "s/\[JOB_NAME\]/$JOB_NAME/" "$NOMAD_JOB_PATH/selenium-grid-hub.hcl" | nomad job run -var="dc=$NOMAD_DC" -
