9877d96007c5c99fb091636b7a1ee6a58b4a74fe
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-09-15 08:53:43 -0500

    task: nomad addr via hostname

--- scripts/deploy-nomad-colibri-proxy.sh
+++ scripts/deploy-nomad-colibri-proxy.sh
@@ -10,24 +10,15 @@ LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
 
 [ -e "$LOCAL_PATH/../sites/$ENVIRONMENT/stack-env.sh" ] && . "$LOCAL_PATH/../sites/$ENVIRONMENT/stack-env.sh"
 
+[ -e "$LOCAL_PATH/../clouds/all.sh" ] && . "$LOCAL_PATH/../clouds/all.sh"
+
 [ -z "$VAULT_PASSWORD_FILE" ] && VAULT_PASSWORD_FILE="$LOCAL_PATH/../.vault-password.txt"
 
+[ -z "$LOCAL_REGION" ] && LOCAL_REGION="$OCI_LOCAL_REGION"
 [ -z "$LOCAL_REGION" ] && LOCAL_REGION="us-phoenix-1"
 
 if [ -z "$NOMAD_ADDR" ]; then
-    NOMAD_IPS="$(DATACENTER="$ENVIRONMENT-$LOCAL_REGION" OCI_DATACENTERS="$ENVIRONMENT-$LOCAL_REGION" ENVIRONMENT="$ENVIRONMENT" FILTER_ENVIRONMENT="false" SHARD='' RELEASE_NUMBER='' SERVICE="nomad-servers" DISPLAY="addresses" $LOCAL_PATH/consul-search.sh ubuntu)"
-    if [ -n "$NOMAD_IPS" ]; then
-        NOMAD_IP="$(echo $NOMAD_IPS | cut -d ' ' -f1)"
-        export NOMAD_ADDR="http://$NOMAD_IP:4646"
-    else
-        echo "No NOMAD_IPS for in environment $ENVIRONMENT in consul"
-        exit 5
-    fi
-fi
-
-if [ -z "$NOMAD_ADDR" ]; then
-    echo "Failed to set NOMAD_ADDR, exiting"
-    exit 5
+    export NOMAD_ADDR="https://$ENVIRONMENT-$LOCAL_REGION-nomad.$TOP_LEVEL_DNS_ZONE_NAME"
 fi
 
 [ -z "$REGIONS" ] && REGIONS="$DRG_PEER_REGIONS"
--- scripts/deploy-nomad-shard.sh
+++ scripts/deploy-nomad-shard.sh
@@ -23,22 +23,11 @@ if [ -z "$SHARD" ]; then
     exit 2
 fi
 
-[ -z "$LOCAL_REGION" ] && LOCAL_REGION="$ORACLE_REGION"
+[ -z "$LOCAL_REGION" ] && LOCAL_REGION="$OCI_LOCAL_REGION"
+[ -z "$LOCAL_REGION" ] && LOCAL_REGION="us-phoenix-1"
 
 if [ -z "$NOMAD_ADDR" ]; then
-    NOMAD_IPS="$(DATACENTER="$ENVIRONMENT-$LOCAL_REGION" OCI_DATACENTERS="$ENVIRONMENT-$LOCAL_REGION" ENVIRONMENT="$ENVIRONMENT" FILTER_ENVIRONMENT="false" SHARD='' RELEASE_NUMBER='' SERVICE="nomad-servers" DISPLAY="addresses" $LOCAL_PATH/consul-search.sh ubuntu)"
-    if [ -n "$NOMAD_IPS" ]; then
-        NOMAD_IP="$(echo $NOMAD_IPS | cut -d ' ' -f1)"
-        export NOMAD_ADDR="http://$NOMAD_IP:4646"
-    else
-        echo "No NOMAD_IPS for in environment $ENVIRONMENT in consul"
-        exit 5
-    fi
-fi
-
-if [ -z "$NOMAD_ADDR" ]; then
-    echo "Failed to set NOMAD_ADDR, exiting"
-    exit 5
+    export NOMAD_ADDR="https://$ENVIRONMENT-$LOCAL_REGION-nomad.$TOP_LEVEL_DNS_ZONE_NAME"
 fi
 
 [ -z "$NOMAD_POOL_TYPE" ] && NOMAD_POOL_TYPE="general"
