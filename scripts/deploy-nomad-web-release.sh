f9dfb5f9b60ed5cd9e1e175e34bd390f75796355
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-09-29 14:06:18 -0500

    back to dash for job name

--- scripts/deploy-nomad-web-release.sh
+++ scripts/deploy-nomad-web-release.sh
@@ -240,4 +240,4 @@ done
 
 export NOMAD_VAR_dc="$NOMAD_DC"
 
-sed -e "s/\[JOB_NAME\]/web_release-${RELEASE_NUMBER}/" "$NOMAD_JOB_PATH/jitsi-meet-web.hcl" | nomad job run -
+sed -e "s/\[JOB_NAME\]/web-release-${RELEASE_NUMBER}/" "$NOMAD_JOB_PATH/jitsi-meet-web.hcl" | nomad job run -
