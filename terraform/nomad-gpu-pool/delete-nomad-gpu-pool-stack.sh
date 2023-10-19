6848314785977c038cea775bd4e5a52c32369f2b
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-10-19 14:12:05 -0500

    task: pool of GPU-enabled instances for nomad

---
+++ terraform/nomad-gpu-pool/delete-nomad-gpu-pool-stack.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+#IF THE CURRENT DIRECTORY HAS stack-env.sh THEN INCLUDE IT
+[ -e ./stack-env.sh ] && . ./stack-env.sh
+
+if [ -z "$ENVIRONMENT" ]; then
+   echo "No ENVIRONMENT provided or found.  Exiting ..."
+   exit 201
+fi
+
+[ -e ./sites/$ENVIRONMENT/stack-env.sh ] && . ./sites/$ENVIRONMENT/stack-env.sh
+
+LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
+
+if [ -z $POOL_TYPE ]; then
+  echo "No POOL_TYPE provided or found. Exiting..."
+  exit 202
+fi
+
+if [ -z $ORACLE_REGION ]; then
+  echo "No ORACLE_REGION provided or found. Exiting..."
+  exit 203
+fi
+
+
+# run as user
+if [ -z "$1" ]; then
+  SSH_USER=$(whoami)
+  echo "SSH user is not defined. We use current user: $SSH_USER"
+else
+  SSH_USER=$1
+  echo "Run ssh as $SSH_USER"
+fi
+
+[ -z "$ROLE" ] && ROLE="nomad"
+[ -z "$POOL_TYPE" ] && POOL_TYPE="general"
+[ -z "$NAME" ] && NAME="$ENVIRONMENT-$ORACLE_REGION-$ROLE-$POOL_TYPE"
+[ -z "$ORACLE_GIT_BRANCH" ] && ORACLE_GIT_BRANCH="master"
+
+[ -e "$LOCAL_PATH/../clouds/oracle.sh" ] && . $LOCAL_PATH/../clouds/oracle.sh
+
+ORACLE_CLOUD_NAME="$ORACLE_REGION-$ENVIRONMENT-oracle"
+[ -e "$LOCAL_PATH/../clouds/${ORACLE_CLOUD_NAME}.sh" ] && . $LOCAL_PATH/../clouds/${ORACLE_CLOUD_NAME}.sh
+
+# find pool and shutdown consul
+# HUB_IPS=$($LOCAL_PATH/../../node.py --role $ROLE --pool_type $POOL_TYPE --region $ORACLE_REGION --environment $ENVIRONMENT --batch)
+# if [ ! -z "$HUB_IPS" ]; then
+#     echo "Found pools for $POOL_TYPE in $ENVIRONMENT $ORACLE_REGION: $HUB_IPS"
+#     for HUB_IP in $HUP_IPS; do
+#         ssh -F $LOCAL_PATH/../../../ssh.config $SSH_USER@$HUB_IP "sudo service nomad stop && sudo service consul stop"
+#         if [ $? -eq 0 ]; then
+#             echo "nomad and consul stopped on instances for $GRID_NAME: $HUB_IP"
+#         else
+#             echo "ERROR stopping consul on hub, recreating this grid cause weird behavior for 3 days, unless the hub is removed from consul"
+#         fi
+#     done
+# fi
+
+[ -z "$S3_PROFILE" ] && S3_PROFILE="oracle"
+[ -z "$S3_STATE_BUCKET" ] && S3_STATE_BUCKET="tf-state-$ENVIRONMENT"
+[ -z "$S3_ENDPOINT" ] && S3_ENDPOINT="https://fr4eeztjonbe.compat.objectstorage.$ORACLE_REGION.oraclecloud.com"
+S3_STATE_BASE="$ENVIRONMENT/nomad-gpu-pool/$POOL_TYPE"
+
+[ -z "$S3_STATE_KEY" ] && S3_STATE_KEY="${S3_STATE_BASE}/terraform.tfstate"
+[ -z "$S3_STATE_KEY_SG" ] && S3_STATE_KEY_SG="${S3_STATE_BASE}/terraform-nomad-sg.tfstate"
+
+[ -z "$DELETE_SECURITY_GROUP" ] && DELETE_SECURITY_GROUP="true"
+[ -z "$DELETE_POOL" ] && DELETE_POOL="true"
+
+TERRAFORM_MAJOR_VERSION=$(terraform -v | head -1  | awk '{print $2}' | cut -d'.' -f1)
+TF_GLOBALS_CHDIR=
+if [[ "$TERRAFORM_MAJOR_VERSION" == "v1" ]]; then
+  TF_GLOBALS_CHDIR="-chdir=$LOCAL_PATH/delete"
+  TF_CLI_ARGS=""
+  TF_POST_PARAMS=
+else
+  TF_POST_PARAMS="$LOCAL_PATH/delete"
+fi
+#The —reconfigure option disregards any existing configuration, preventing migration of any existing state
+
+
+if [ "$DELETE_POOL" == "true" ]; then
+    terraform $TF_GLOBALS_CHDIR init \
+        -backend-config="bucket=$S3_STATE_BUCKET" \
+        -backend-config="key=$S3_STATE_KEY" \
+        -backend-config="region=$ORACLE_REGION" \
+        -backend-config="profile=$S3_PROFILE" \
+        -backend-config="endpoint=$S3_ENDPOINT" \
+        -reconfigure $TF_POST_PARAMS
+
+    terraform $TF_GLOBALS_CHDIR destroy \
+    -var="oracle_region=$ORACLE_REGION" \
+    -var="tenancy_ocid=$TENANCY_OCID" \
+    -auto-approve $TF_POST_PARAMS
+fi
+
+
+if [ "$DELETE_SECURITY_GROUP" == "true" ]; then
+    terraform $TF_GLOBALS_CHDIR init \
+        -backend-config="bucket=$S3_STATE_BUCKET" \
+        -backend-config="key=$S3_STATE_KEY_SG" \
+        -backend-config="region=$ORACLE_REGION" \
+        -backend-config="profile=$S3_PROFILE" \
+        -backend-config="endpoint=$S3_ENDPOINT" \
+        -reconfigure $TF_POST_PARAMS
+
+    terraform $TF_GLOBALS_CHDIR destroy \
+    -var="oracle_region=$ORACLE_REGION" \
+    -var="tenancy_ocid=$TENANCY_OCID" \
+    -auto-approve $TF_POST_PARAMS
+fi
+
---
+++ terraform/nomad-gpu-pool/nomad-gpu-pool-stack.tf
@@ -0,0 +1,194 @@
+variable "environment" {}
+variable "name" {}
+variable "oracle_region" {}
+variable "availability_domains" {
+  type = list(string)
+}
+variable "role" {}
+variable "pool_type" {}
+variable "git_branch" {}
+variable "tenancy_ocid" {}
+variable "compartment_ocid" {}
+variable "resource_name_root" {}
+variable "instance_config_name" {}
+variable "image_ocid" {}
+variable "user_public_key_path" {}
+variable "security_group_id" {}
+variable "shape" {}
+variable "memory_in_gbs" {}
+variable "ocpus" {}
+variable "pool_subnet_ocid" {}
+variable "public_subnet_ocid" {}
+variable "instance_pool_size" {}
+variable "instance_pool_name" {}
+variable "environment_type" {}
+variable "tag_namespace" {}
+variable "user" {}
+variable "user_private_key_path" {}
+variable "vcn_name" {}
+
+variable "postinstall_status_file" {}
+variable "user_data_lib_path" {
+  default = "terraform/lib"
+}
+variable "user_data_file" {
+  default = "terraform/nomad-pool/user-data/postinstall-runner-oracle.sh"
+}
+variable "infra_configuration_repo" {}
+variable "infra_customizations_repo" {}
+
+locals {
+  common_freeform_tags = {
+    "pool_type" = var.pool_type
+    shape = var.shape
+  }
+  common_tags = {
+    "${var.tag_namespace}.environment" = var.environment
+    "${var.tag_namespace}.environment_type" = var.environment_type
+    "${var.tag_namespace}.git_branch" = var.git_branch
+    "${var.tag_namespace}.role" = var.role
+    "${var.tag_namespace}.Name" = var.name
+  }
+}
+
+provider "oci" {
+  region = var.oracle_region
+  tenancy_ocid = var.tenancy_ocid
+}
+
+terraform {
+  backend "s3" {
+    skip_region_validation = true
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+    force_path_style = true
+  }
+  required_providers {
+      oci = {
+          source  = "oracle/oci"
+      }
+  }
+}
+
+resource "oci_core_instance_configuration" "oci_instance_configuration" {
+  lifecycle {
+      create_before_destroy = true
+  }
+
+  compartment_id = var.compartment_ocid
+  display_name = var.instance_config_name
+
+  freeform_tags = local.common_freeform_tags
+  defined_tags = local.common_tags
+
+  instance_details {
+    instance_type = "compute"
+
+    launch_details {
+      compartment_id = var.compartment_ocid
+      shape = var.shape
+
+      shape_config {
+        memory_in_gbs = var.memory_in_gbs
+        ocpus = var.ocpus
+      }
+
+      create_vnic_details {
+        subnet_id = var.pool_subnet_ocid
+        nsg_ids = [var.security_group_id]
+      }
+
+      source_details {
+        source_type = "image"
+        image_id = var.image_ocid
+      }
+
+      metadata = {
+        user_data = base64encode(join("",[
+          file("${path.cwd}/${var.user_data_lib_path}/postinstall-header.sh"), # load the header
+          file("${path.cwd}/${var.user_data_lib_path}/postinstall-lib.sh"), # load the lib
+          "\nexport INFRA_CONFIGURATION_REPO=${var.infra_configuration_repo}\nexport INFRA_CUSTOMIZATIONS_REPO=${var.infra_customizations_repo}\n", #repo variables
+          file("${path.cwd}/${var.user_data_file}"), # load our customizations
+          file("${path.cwd}/${var.user_data_lib_path}/postinstall-footer.sh") # load the footer
+        ]))
+        ssh_authorized_keys = file(var.user_public_key_path)
+      }
+
+      freeform_tags = local.common_freeform_tags
+      defined_tags = local.common_tags
+    }
+  }
+}
+
+data "oci_core_vcns" "vcns" {
+  compartment_id = var.compartment_ocid
+  display_name = var.vcn_name
+}
+
+resource "oci_core_instance_pool" "oci_instance_pool" {
+  compartment_id = var.compartment_ocid
+  instance_configuration_id = oci_core_instance_configuration.oci_instance_configuration.id  
+  display_name = var.instance_pool_name
+  size = var.instance_pool_size
+
+  dynamic "placement_configurations" {
+    for_each = toset(var.availability_domains)
+    content {
+      primary_subnet_id = var.pool_subnet_ocid
+      availability_domain = placement_configurations.value
+    }
+  }
+
+  freeform_tags = local.common_freeform_tags
+  defined_tags = local.common_tags
+}
+
+data "oci_core_instance_pool_instances" "oci_instance_pool_instances" {
+  compartment_id = var.compartment_ocid
+  instance_pool_id = oci_core_instance_pool.oci_instance_pool.id
+  depends_on = [oci_core_instance_pool.oci_instance_pool]
+}
+
+data "oci_core_instance" "oci_instance_datasources" {
+  count = var.instance_pool_size
+  instance_id = lookup(data.oci_core_instance_pool_instances.oci_instance_pool_instances.instances[count.index], "id")
+}
+
+locals {
+  private_ips = data.oci_core_instance.oci_instance_datasources.*.private_ip
+}
+
+resource "null_resource" "verify_cloud_init" {
+  count = var.instance_pool_size
+  depends_on = [data.oci_core_instance.oci_instance_datasources]
+
+  provisioner "remote-exec" {
+    inline = [
+      "cloud-init status --wait"
+    ]
+    connection {
+      type = "ssh"
+      host = element(local.private_ips, count.index)
+      user = var.user
+      private_key = file(var.user_private_key_path)
+
+      script_path = "/home/${var.user}/script_%RAND%.sh"
+
+      timeout = "10m"
+    }
+  }
+}
+
+resource "null_resource" "cloud_init_output" {
+  count = var.instance_pool_size
+  depends_on = [null_resource.verify_cloud_init]
+
+  provisioner "local-exec" {
+    command = "ssh -o StrictHostKeyChecking=no ${var.user}@${element(local.private_ips, count.index)} 'echo hostname: $HOSTNAME, privateIp: ${element(local.private_ips, count.index)} - $(cloud-init status)' >> ${var.postinstall_status_file}"
+  }
+}
+
+
+output "private_ips" {
+  value = local.private_ips
+}
