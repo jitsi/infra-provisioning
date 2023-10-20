17be6976a236457787b8072bb7ef431c9df490e9
Aaron van Meerten <aaron.van.meerten@gmail.com>
2023-10-20 15:18:31 -0500

    task: tag images with GPU support (#200)

---
+++ terraform/nomad-gpu-pool/security-group/security-group.tf
@@ -0,0 +1,150 @@
+variable "resource_name_root" {}
+variable "vcn_name" {}
+variable "oracle_region" {}
+variable "tenancy_ocid" {}
+variable "compartment_ocid" {}
+variable "ephemeral_ingress_cidr" {
+  default = "10.0.0.0/8"
+}
+
+provider "oci" {
+  region = var.oracle_region
+  tenancy_ocid = var.tenancy_ocid
+}
+
+terraform {
+  backend "s3" {
+    skip_region_validation = true
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+    force_path_style = true
+  }
+  required_providers {
+      oci = {
+          source  = "oracle/oci"
+      }
+  }
+}
+
+data "oci_core_vcns" "vcns" {
+  compartment_id = var.compartment_ocid
+  display_name = var.vcn_name
+}
+
+resource "oci_core_network_security_group" "security_group" {
+  compartment_id = var.compartment_ocid
+  vcn_id = data.oci_core_vcns.vcns.virtual_networks[0].id
+  display_name = "${var.resource_name_root}-SecurityGroup"
+}
+
+resource "oci_core_network_security_group_security_rule" "nsg_rule_egress" {
+  network_security_group_id = oci_core_network_security_group.security_group.id
+  direction = "EGRESS"
+  destination = "0.0.0.0/0"
+  protocol = "all"
+}
+
+resource "oci_core_network_security_group_security_rule" "nsg_rule_ingress_ssh" {
+  network_security_group_id = oci_core_network_security_group.security_group.id
+  direction = "INGRESS"
+  protocol = "6"
+  source = data.oci_core_vcns.vcns.virtual_networks[0].cidr_block
+  stateless = false
+
+  tcp_options {
+    destination_port_range {
+      max = 22
+      min = 22
+    }
+  }
+}
+
+resource "oci_core_network_security_group_security_rule" "nsg_rule_ingress_nomad_tcp" {
+  network_security_group_id = oci_core_network_security_group.security_group.id
+  direction = "INGRESS"
+  protocol = "6"
+  source = "10.0.0.0/8"
+  stateless = false
+
+  tcp_options {
+    destination_port_range {
+      min = 4646
+      max = 4647
+    }
+  }
+}
+
+resource "oci_core_network_security_group_security_rule" "nsg_rule_ingress_nomad_ingress" {
+  network_security_group_id = oci_core_network_security_group.security_group.id
+  direction = "INGRESS"
+  protocol = "6"
+  source = "10.0.0.0/8"
+  stateless = false
+
+  tcp_options {
+    destination_port_range {
+      min = 9996
+      max = 9999
+    }
+  }
+}
+
+resource "oci_core_network_security_group_security_rule" "nsg_rule_ingress_nomad_ephemeral_tcp" {
+  network_security_group_id = oci_core_network_security_group.security_group.id
+  direction = "INGRESS"
+  protocol = "6"
+  source = var.ephemeral_ingress_cidr
+  stateless = false
+
+  tcp_options {
+    destination_port_range {
+      min = 20000
+      max = 32000
+    }
+  }
+}
+
+resource "oci_core_network_security_group_security_rule" "nsg_rule_ingress_nomad_ephemeral_udp" {
+  network_security_group_id = oci_core_network_security_group.security_group.id
+  direction = "INGRESS"
+  protocol = "17"
+  source = var.ephemeral_ingress_cidr
+  stateless = false
+
+  udp_options {
+    destination_port_range {
+      min = 20000
+      max = 32000
+    }
+  }
+}
+
+resource "oci_core_network_security_group_security_rule" "nsg_rule_ingress_consul_serf_tcp" {
+  network_security_group_id = oci_core_network_security_group.security_group.id
+  direction = "INGRESS"
+  protocol = "6"
+  source = data.oci_core_vcns.vcns.virtual_networks[0].cidr_block
+  stateless = false
+
+  tcp_options {
+    destination_port_range {
+      max = 8301
+      min = 8301
+    }
+  }
+}
+
+resource "oci_core_network_security_group_security_rule" "nsg_rule_ingress_consul_serf_udp" {
+  network_security_group_id = oci_core_network_security_group.security_group.id
+  direction = "INGRESS"
+  protocol = "17"
+  source = data.oci_core_vcns.vcns.virtual_networks[0].cidr_block
+  stateless = false
+
+  udp_options {
+    destination_port_range {
+      min = 8301
+      max = 8301
+    }
+  }
+}
---
+++ terraform/nomad-gpu-pool/user-data/postinstall-runner-oracle.sh
@@ -0,0 +1,16 @@
+# nomad customizations
+
+# with no overrides, provision based on the variables below using default_provision
+# default_provision takes the following variables ANSIBLE_PLAYBOOK, ANSIBLE_VARS and HOST_ROLE (defaults to $SHARD_ROLE)
+export ANSIBLE_PLAYBOOK="nomad-client.yml"
+
+. /usr/local/bin/oracle_cache.sh
+[ -z "$CACHE_PATH" ] && CACHE_PATH=$(ls /tmp/oracle_cache-*)
+export POOL_TYPE_TAG="pool_type"
+export POOL_TYPE=$(cat $CACHE_PATH | jq -r --arg POOL_TYPE_TAG "$POOL_TYPE_TAG" ".[\"$POOL_TYPE_TAG\"]")
+if [[ "$POOL_TYPE" == "null" ]]; then
+    export POOL_TYPE=
+fi
+[ -z "$POOL_TYPE" ] && POOL_TYPE="skynet"
+export HOST_ROLE="gpu"
+export ANSIBLE_VARS="hcv_environment=$ENVIRONMENT cloud_name=$CLOUD_NAME cloud_provider=oracle oracle_region=$ORACLE_REGION region=$ORACLE_REGION nomad_pool_type=$POOL_TYPE nvidia_docker_flag=true"
