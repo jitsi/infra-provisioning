641206f7f97a2f627a47d2b6acf96c4dae103d79
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-11-15 11:47:19 -0600

    script to deploy

---
+++ scripts/deploy-nomad-nvidia-prom-exporter.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ -z "$ENVIRONMENT" ]; then
+    echo "No ENVIRONMENT set, exiting"
+    exit 2
+fi
+
+LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
+
+[ -e "$LOCAL_PATH/../sites/$ENVIRONMENT/stack-env.sh" ] && . "$LOCAL_PATH/../sites/$ENVIRONMENT/stack-env.sh"
+
+[ -e "$LOCAL_PATH/../clouds/all.sh" ] && . "$LOCAL_PATH/../clouds/all.sh"
+
+[ -z "$REGIONS" ] && REGIONS="$DRG_PEER_REGIONS"
+
+NOMAD_JOB_PATH="$LOCAL_PATH/../nomad"
+
+NOMAD_DC="[]"
+for ORACLE_REGION in $REGIONS; do
+    NOMAD_DC="$( echo "$NOMAD_DC" "[\"$ENVIRONMENT-$ORACLE_REGION\"]" | jq -c -s '.|add')"
+done
+
+[ -z "$LOCAL_REGION" ] && LOCAL_REGION="$OCI_LOCAL_REGION"
+[ -z "$LOCAL_REGION" ] && LOCAL_REGION="us-phoenix-1"
+
+if [ -z "$NOMAD_ADDR" ]; then
+    export NOMAD_ADDR="https://$ENVIRONMENT-$LOCAL_REGION-nomad.$TOP_LEVEL_DNS_ZONE_NAME"
+fi
+
+export NOMAD_VAR_dc="$NOMAD_DC"
+
+nomad job run $NOMAD_JOB_PATH/nvidia-prom-exporter.hcl
+exit $?
