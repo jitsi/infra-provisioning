2c7013261e530cc1993d0f3c34afc13b5d0196f2
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-12-04 14:14:50 -0600

    pass rendered job into nomad bin

--- scripts/deploy-nomad-web-release.sh
+++ scripts/deploy-nomad-web-release.sh
@@ -493,8 +493,8 @@ for ORACLE_REGION in $REGIONS; do
 done
 
 export NOMAD_VAR_dc="$NOMAD_DC"
-cd nomad/jitsi_packs/packs/jitsi_meet_web
-nomad-pack run --name "web-release-${RELEASE_NUMBER}"  -var "job_name=web-release-${RELEASE_NUMBER}" .
+
+nomad-pack render --name "web-release-${RELEASE_NUMBER}"  -var "job_name=web-release-${RELEASE_NUMBER}" nomad/jitsi_packs/packs/jitsi_meet_web | tail -n+2 | nomad job run -
 #levant render -var "web_release.job_name=web-release-${RELEASE_NUMBER}" $NOMAD_JOB_PATH/jitsi-meet-web.hcl | nomad job run -
 
 # sed -e "s/\[JOB_NAME\]/web-release-${RELEASE_NUMBER}/" "$NOMAD_JOB_PATH/jitsi-meet-web.hcl" | nomad job run -
