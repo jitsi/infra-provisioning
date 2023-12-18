80a1e0c41f9328ee2818a379ebb6a690775738b0
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-12-18 10:53:59 -0600

    enable global pools by default in nomad

--- scripts/get-jvb-pool-modes-for-environment.sh
+++ scripts/get-jvb-pool-modes-for-environment.sh
@@ -12,9 +12,17 @@ fi
 
 [ -e ./sites/$ENVIRONMENT/stack-env.sh ] && . ./sites/$ENVIRONMENT/stack-env.sh
 
-[ -z "$ENABLE_JVB_GLOBAL_POOLS" ] && ENABLE_JVB_GLOBAL_POOLS="false"
-[ -z "$ENABLE_JVB_LOCAL_POOLS" ] && ENABLE_JVB_LOCAL_POOLS="false"
-[ -z "$ENABLE_JVB_REMOTE_POOLS" ] && ENABLE_JVB_REMOTE_POOLS="false"
+[ -z "$CLOUD_PROVIDER" ] && CLOUD_PROVIDER="oracle"
+
+if [[ "$CLOUD_PROVIDER" == "nomad" ]]; then
+    [ -z "$ENABLE_JVB_GLOBAL_POOLS" ] && ENABLE_JVB_GLOBAL_POOLS="true"
+    [ -z "$ENABLE_JVB_LOCAL_POOLS" ] && ENABLE_JVB_LOCAL_POOLS="false"
+    [ -z "$ENABLE_JVB_REMOTE_POOLS" ] && ENABLE_JVB_REMOTE_POOLS="false"
+else
+    [ -z "$ENABLE_JVB_GLOBAL_POOLS" ] && ENABLE_JVB_GLOBAL_POOLS="false"
+    [ -z "$ENABLE_JVB_LOCAL_POOLS" ] && ENABLE_JVB_LOCAL_POOLS="false"
+    [ -z "$ENABLE_JVB_REMOTE_POOLS" ] && ENABLE_JVB_REMOTE_POOLS="false"
+fi
 
 OUT=""
 
