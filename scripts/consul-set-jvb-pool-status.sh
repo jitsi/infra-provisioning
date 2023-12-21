dccad59be53fc5c84cea3dba64586653a718671e
Aaron van Meerten <aaron.van.meerten@8x8.com>
2023-12-21 10:49:11 -0600

    task: jvb pool status stops using ssh

--- scripts/consul-set-jvb-pool-status.sh
+++ scripts/consul-set-jvb-pool-status.sh
@@ -13,13 +13,6 @@ LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
 
 JVB_POOL_STATUS=$1
 
-if [  -z "$2" ]
-then
-  ANSIBLE_SSH_USER=$(whoami)
-else
-  ANSIBLE_SSH_USER=$2
-fi
-
 if [ -z "$ORACLE_REGION" ]; then
     echo "No ORACLE_REGION set, exiting"
     exit 1
@@ -42,21 +35,14 @@ fi
 OCI_LOCAL_DATACENTER="$ENVIRONMENT-$OCI_LOCAL_REGION"
 OCI_DATACENTER="$ENVIRONMENT-$ORACLE_REGION"
 
-PORT_OCI=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
-
-ssh -fNT -L127.0.0.1:$PORT_OCI:$OCI_LOCAL_DATACENTER-consul.$TOP_LEVEL_DNS_ZONE_NAME:443 $ANSIBLE_SSH_USER@$OCI_LOCAL_REGION-$ENVIRONMENT-ssh.$DEFAULT_DNS_ZONE_NAME
-
-OCI_CONSUL_HOST="consul-local.$TOP_LEVEL_DNS_ZONE_NAME"
-OCI_CONSUL_URL="https://$OCI_CONSUL_HOST:$PORT_OCI"
+OCI_CONSUL_URL="https://$OCI_LOCAL_DATACENTER-consul.$TOP_LEVEL_DNS_ZONE_NAME"
 KV_URL="$OCI_CONSUL_URL/v1/kv/pool-states/$ENVIRONMENT/$JVB_POOL_NAME?dc=$OCI_DATACENTER"
 
-RESPONSE=$(curl --resolve $OCI_CONSUL_HOST:$PORT_OCI:127.0.0.1 -d"$JVB_POOL_STATUS" -X PUT $KV_URL)
+RESPONSE=$(curl -d"$JVB_POOL_STATUS" -X PUT $KV_URL)
 if [ $? -eq 0 ]; then
     FINAL_RET=0
 else
     FINAL_RET=2
 fi
-SSH_OCI_PID=$(ps auxww | grep "ssh \-fNT -L127.0.0.1:$PORT_OCI" | awk '{print $2}')
-kill $SSH_OCI_PID
 
 exit $FINAL_RET
