6f75af7f44960ac38e28b34431c56078d0c12738
Aaron van Meerten <aaron.van.meerten@8x8.com>
2024-06-21 10:10:21 -0400

    fix s3 backend

--- terraform/vcn-security-lists/security-lists.tf
+++ terraform/vcn-security-lists/security-lists.tf
@@ -13,6 +13,20 @@ provider "oci" {
   tenancy_ocid = var.tenancy_ocid
 }
 
+terraform {
+  backend "s3" {
+    skip_region_validation = true
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+    force_path_style = true
+  }
+  required_providers {
+      oci = {
+          source  = "oracle/oci"
+      }
+  }
+}
+
 data "oci_core_vcns" "vcns" {
   compartment_id = var.compartment_ocid
   display_name = var.vcn_name
