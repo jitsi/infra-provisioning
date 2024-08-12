48329f10baad5304d1b6565cb41075332623028d
Aaron van Meerten <aaron.van.meerten@8x8.com>
2024-08-12 15:03:44 -0400

    feat(boot): source postinstall from disk

--- terraform/lib/postinstall-header.sh
+++ terraform/lib/postinstall-header.sh
@@ -4,3 +4,4 @@ EXIT_CODE=0
 tmp_msg_file='/tmp/postinstall_runner_message'
 TIMEOUT_BIN="/usr/bin/timeout"
 PROVISIONING_TIMEOUT="1200"
+[ -e "/opt/jitsi/boot/postinstall-lib.sh" ] && . /opt/jitsi/boot/postinstall-lib.sh
