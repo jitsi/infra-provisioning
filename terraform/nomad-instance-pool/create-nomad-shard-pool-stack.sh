72adbfac4ad486f263178a9e691f1ed4e15e12e1
Aaron van Meerten <aaron.van.meerten@gmail.com>
2024-10-10 14:26:22 -0500

    feat(shard): 2 nomad shard pool instances by default (#654)

--- terraform/nomad-instance-pool/create-nomad-shard-pool-stack.sh
+++ terraform/nomad-instance-pool/create-nomad-shard-pool-stack.sh
@@ -3,7 +3,9 @@ set -x
 unset SSH_USER
 
 [ -z "$POOL_TYPE" ] && export POOL_TYPE="shard"
-[ -z "$DISK_IN_GBS" ] && DISK_IN_GBS="50"
+[ -z "$DISK_IN_GBS" ] && export DISK_IN_GBS="50"
+# at least 2 for shard pools
+[ -z "$INSTANCE_POOL_SIZE" ] && export INSTANCE_POOL_SIZE=2
 
 LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
 $LOCAL_PATH/create-nomad-instance-pool-stack.sh $@
