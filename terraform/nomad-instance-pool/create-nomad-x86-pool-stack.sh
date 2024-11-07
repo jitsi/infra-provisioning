d274c007ecf4ec28b613309f9c98124d6bfd695e
scott boone <scott.e.boone@gmail.com>
2024-11-07 11:19:50 -0600

    change x86 pool default to e4

--- terraform/nomad-instance-pool/create-nomad-x86-pool-stack.sh
+++ terraform/nomad-instance-pool/create-nomad-x86-pool-stack.sh
@@ -3,7 +3,7 @@ set -x
 unset SSH_USER
 
 [ -z "$POOL_TYPE" ] && export POOL_TYPE="x86"
-[ -z "$SHAPE" ] && export SHAPE="VM.Standard.E5.Flex"
+[ -z "$SHAPE" ] && export SHAPE="VM.Standard.E4.Flex"
 [ -z "$DISK_IN_GBS" ] && export DISK_IN_GBS="50"
 
 LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
