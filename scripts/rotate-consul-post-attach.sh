3eefd07ddda463d39619035a2af11252ae2bb46f
Aaron van Meerten <aaron.van.meerten@8x8.com>
2024-12-31 13:03:50 -0600

    archive and distribute nomad keyring during rotation

--- scripts/rotate-consul-post-attach.sh
+++ scripts/rotate-consul-post-attach.sh
@@ -8,5 +8,22 @@ else
 fi
 
 LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
+
+# find the instance from the load balancer (should only be one for consul)
+# grab the ip from the load-balancer-backends field
+INSTANCE_PRIMARY_PRIVATE_IP="$(oci compute-management instance-pool list-instances --compartment-id "$COMPARTMENT_OCID" \
+  --instance-pool-id "$INSTANCE_POOL_ID" --region "$ORACLE_REGION" --all | \
+  jq -r '.data[0]."load-balancer-backends"[0]."backend-name"|split(":")[0]')"
+
+if [ -f "./nomad-keyring.tar.gz" ]; then
+  echo "## rotate-consul-post-attach: copying nomad keyring material to $INSTANCE_PRIMARY_PRIVATE_IP" 
+  scp -F $LOCAL_PATH/../config/ssh.config ./nomad-keyring.tar.gz $SSH_USER@$INSTANCE_PRIMARY_PRIVATE_IP:/tmp/nomad-keyring.tar.gz
+  echo "## rotate-consul-post-attach: extracting nomad keyring material on $INSTANCE_PRIMARY_PRIVATE_IP"
+  timeout 120 ssh -F $LOCAL_PATH/../config/ssh.config $SSH_USER@$INSTANCE_PRIMARY_PRIVATE_IP "sudo tar -xzf /tmp/nomad-keyring.tar.gz -C / && sudo rm -f /tmp/nomad-keyring.tar.gz"
+  rm ./nomad-keyring.tar.gz
+else 
+  echo "## rotate-consul-post-attach: no nomad keyring material to copy, skipping copy and extraction"
+fi
+
 echo "## rotate-consul-post-attach re-running terraform"
 $LOCAL_PATH/../terraform/consul-server/create-consul-server-oracle.sh $SSH_USER
--- scripts/rotate-consul-pre-detach.sh
+++ scripts/rotate-consul-pre-detach.sh
@@ -17,6 +17,28 @@ fi
 
 LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
 
+# first archive the keyring material from the old server if nomad version is less than 1.9
+echo "## rotate-consul-pre-detach: checking for nomad keyring material from $INSTANCE_PRIMARY_PRIVATE_IP"
+export ARCHIVE_KEYRING="true"
+timeout 120 ssh -F $LOCAL_PATH/../config/ssh.config $SSH_USER@$INSTANCE_PRIMARY_PRIVATE_IP  "[ $(nomad --version | head -1 | awk '{print $2}' | cut -d '.' -f2) -ge 9 ] && echo 'nomad version is 1.9 or greater, no keyring needed' && exit 1 || sudo tar -czf /tmp/nomad-keyring.tar.gz /var/nomad/server/keystore"
+RET=$?
+if [[ $RET -gt 0 ]]; then
+    if [ $RET -eq 1 ]; then
+        echo "## rotate-consul-pre-detach: nomad version is 1.9 or greater, no keyring archival needed"
+        export ARCHIVE_KEYRING="false"
+    else
+        echo "## ERROR archiving nomad keyring material on $INSTANCE_PRIMARY_PRIVATE_IP with code $RET"
+    fi
+fi
+
+if [ "$ARCHIVE_KEYRING" == "true" ]; then
+    echo "## rotate-consul-pre-detach: copying nomad keyring material from $INSTANCE_PRIMARY_PRIVATE_IP"
+    scp -F $LOCAL_PATH/../config/ssh.config $SSH_USER@$INSTANCE_PRIMARY_PRIVATE_IP:/tmp/nomad-keyring.tar.gz ./nomad-keyring.tar.gz || echo "## failed to copy nomad keyring material from $INSTANCE_PRIMARY_PRIVATE_IP"
+    ## now delete the remote copy
+    echo "## rotate-consul-pre-detach: deleting /tmp archived nomad keyring material from $INSTANCE_PRIMARY_PRIVATE_IP"
+    timeout 120 ssh -F $LOCAL_PATH/../config/ssh.config $SSH_USER@$INSTANCE_PRIMARY_PRIVATE_IP "sudo rm -f /tmp/nomad-keyring.tar.gz"
+fi
+
 echo "## rotate-consul-pre-detach shutting down nomad service on $INSTANCE_PRIMARY_PRIVATE_IP with user $SSH_USER"
 
 timeout 10 ssh -n -o StrictHostKeyChecking=no -F $LOCAL_PATH/../config/ssh.config $SSH_USER@$INSTANCE_PRIMARY_PRIVATE_IP "nomad node eligibility -self -disable && nomad node drain -self -enable -detach -yes"
