384d88794983dd7a969fcf28c11cb72f1079f19d
damencho <damencho@jitsi.org>
2025-02-25 12:23:01 -0600

    feat: Validate shards using the new jitsi-meet tests.
    squash: Correct folder for results.
    
    fix: fix base url.

--- scripts/validate-shard.sh
+++ scripts/validate-shard.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
-unset ANSIBLE_SSH_USER
-set -x #echo on
 
-if [  -z "$1" ]
-then
-  ANSIBLE_SSH_USER=$(whoami)
-  echo "Ansible SSH user is not defined. We use current user: $ANSIBLE_SSH_USER"
-else
-  ANSIBLE_SSH_USER=$1
-  echo "Run ansible as $ANSIBLE_SSH_USER"
-fi
+# make sure NVM is setup correctly
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+set -x #echo on
 
 LOCAL_PATH=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 [ -z "$ANSIBLE_BUILD_PATH" ] && ANSIBLE_BUILD_PATH="$LOCAL_PATH/../../infra-configuration"
@@ -36,46 +31,36 @@ fi
 #pull in cloud-specific variables
 [ -e "$LOCAL_PATH/../clouds/${CLOUD_NAME}.sh" ] && . $LOCAL_PATH/../clouds/${CLOUD_NAME}.sh
 
-
 CMD_SHARD_NUMBER=$(echo $0 | cut -d'-' -f3 | cut -d'.' -f1)
 
-[ -z $TEST_DURATION ] && TEST_DURATION=10
-
 [ -z $SHARD_NUMBER ] && SHARD_NUMBER=$CMD_SHARD_NUMBER
-
-[ -z $TORTURE_COUNT ] && TORTURE_COUNT=1
 
 [ -z $SHARD ] && SHARD="${SHARD_BASE}-${REGION_ALIAS}${JVB_AZ_LETTER}-s${SHARD_NUMBER}"
 
 [ -z $TEST_DOMAIN ] && TEST_DOMAIN=$DOMAIN
 
-[ -z $TORTURE_LONG ] && TORTURE_LONG='all'
-
-[ -z $TEST_ID ] && TEST_ID='standalone'
-
 #jenkins build number, we get it from jenkins
 [ -z $BUILD_NUMBER ] && BUILD_NUMBER='N/A'
 
-SHARD_IP=$(IP_TYPE="external" SHARD="$SHARD" $LOCAL_PATH/shard.sh shard_ip $ANSIBLE_SSH_USER)
-TEST_SUBDIR=''
+VAULT_KEY="$ENVIRONMENT/asap/validation-tests"
 
-if [ -z $SHARD_IP ]; then
-    SHARD_CORE_PROVIDER="$(SHARD=$SHARD ENVIRONMENT=$ENVIRONMENT $LOCAL_PATH/shard.sh core_provider $ANSIBLE_SSH_USER)"
-    if [[ "$SHARD_CORE_PROVIDER" == "nomad" ]]; then
-      SHARD_REGION=$($LOCAL_PATH/shard.py --shard_region --environment=$ENVIRONMENT --shard=$SHARD)
-      [ -z "$NOMAD_LB_HOSTNAME" ] && NOMAD_LB_HOSTNAME="$ENVIRONMENT-$SHARD_REGION-nomad-pool-general.$ORACLE_DNS_ZONE_NAME"
+# login to vault to fetch the keypair
+. $LOCAL_PATH/vault-login.sh
 
-      SHARD_IP=$(dig +short "$NOMAD_LB_HOSTNAME")
-      if [ -z $SHARD_IP ]; then
-        echo "No nomad LB IP found, failing to run tests."
-        exit 1
-      fi
-      TEST_SUBDIR="/$SHARD"
-    else
-      echo "No shard IP found, failing to run tests."
-      exit 1
-    fi
-fi
+KEYPAIR_PATH="./keypair.json"
+# fetch the keypair from vault
+set +x
+vault kv get -format=json -mount=secret $VAULT_KEY | jq -r '.data.data' > $KEYPAIR_PATH
+
+JAAS_JWT_KID="$(jq -r '.key_id' $KEYPAIR_PATH)"
+JAAS_WH_SHARED_SECRET="$(jq -r '.webhook_shared_secret' $KEYPAIR_PATH)"
+
+JAAS_SIGNING_KEY_FILE=$(realpath "./jaas.key")
+
+echo "Using JAAS_SIGNING_KEY_FILE: $JAAS_SIGNING_KEY_FILE"
+
+jq -r '.private_key' $KEYPAIR_PATH | sed 's/\\n/\n/g' > $JAAS_SIGNING_KEY_FILE
+rm $KEYPAIR_PATH
 
 #https://web-cdn.jitsi.net/meet8x8com_4570.1272/
 #https://web-cdn.jitsi.net/meetjitsi_4628.1277/
@@ -87,113 +72,85 @@ fi
 # - https://web-cdn.jitsi.net/meetjitsi_4628.1277/ for meet.jit.si
 # - https://web-cdn.jitsi.net/4679/ for those without branding as beta.meet.jit.si
 #
-# with jitsi-meet-web version we check all available meta packages from the newest
-# to the latest for that web version and when we find that one
-# we use the meta version to construct the tag to use for jitsi-meet-torture
-function getJitsiMeetTortureTag() {
-  SERVER_IP=$1
-  SERVER_DIR=$2
-  BASE_HTML=$(curl --silent --insecure https://${SERVER_IP}${SERVER_DIR}/base.html)
+function getJitsiMeetTag() {
+  SHARD_NAME=$1
+
+  BASE_HTML=$(curl --silent --insecure https://${DOMAIN}/${SHARD_NAME}/base.html)
   WEB_FULL_VER=$(echo $BASE_HTML | sed 's|.*web-cdn.jitsi.net/||' | sed "s|.*${DOMAIN}/v1/_cdn/||" | sed 's|/".*||')
   WEB_VER=$(echo $WEB_FULL_VER | sed 's|.*_|| ' | sed 's|\..*||')
 
-  JITSI_MEET_VERSIONS=$(apt-cache madison jitsi-meet | awk '{print $3;}' | cut -d'-' -f1,2,3)
-  for item in $JITSI_MEET_VERSIONS
-  do
-      current_ver=$(apt-cache show jitsi-meet=$item | grep '^Depends:'  | cut -f2- -d: | cut -f2 -d,)
-      if grep -q ".${WEB_VER}-1" <<< "$current_ver"; then
-          #jitsi-meet-web version ${WEB_VER} is in jitsi-meet (meta) $item
-          BUILD_NUM=$(echo $item | sed -n "s/[0-9]*\.[0-9]*\.\([0-9]*\)-1/\1/p")
-          #"The tag is jitsi-meet_${BUILD_NUM}"
-          echo "jitsi-meet_${BUILD_NUM}";
-    break
-      fi
-  done
-
-  [ -z "$BUILD_NUM" ] && echo "master";
+#  echo "${WEB_VER}";
+  echo "validate"; # TODO: remove this line
 }
-
-if [ -z $TORTURE_BRANCH ]; then
-  TORTURE_BRANCH=$(getJitsiMeetTortureTag $SHARD_IP $TEST_SUBDIR)
-fi
-
-[ -z $TORTURE_BRANCH ] && TORTURE_BRANCH="master"
-
-[ -z "$TORTURE_PATH" ] && TORTURE_PATH="../test-results/$TEST_ID"
-
-[ -d "$TORTURE_PATH" ] || mkdir -p $TORTURE_PATH
 
 # generate a token if a client key file is defined
 if [ -n "$ASAP_CLIENT_SIGNING_KEY_FILE" ]; then
-  TOKEN=$($LOCAL_PATH/generate-client-token.sh | tail -1)
+  export JWT_ACCESS_TOKEN=$($LOCAL_PATH/generate-client-token.sh | tail -1)
 fi
-
-
-#clean up results from any previous tests
-rm -rf $TORTURE_PATH/*
-
-usage() { echo "Usage: $0 [<username>]" 1>&2; }
-
-usage
-
-if [  -z "$2" ]
-then
-  EC2_SSH_KEYPAIR=betadeploy
-  echo "Ansible SSH keypair is not defined. We use default keypair: $EC2_SSH_KEYPAIR"
-else
-  EC2_SSH_KEYPAIR=$2
-  echo "Building nodes with SSH keypair $EC2_SSH_KEYPAIR"
-fi
-
-DEPLOY_TAGS=${ANSIBLE_TAGS-"all"}
 
 #first we set the shard state to "testing"
 $LOCAL_PATH/set_shard_tested.py $ENVIRONMENT $SHARD testing $BUILD_NUMBER
 
-cd $ANSIBLE_BUILD_PATH
+export TMPDIR=$(mktemp -d)
+function cleanup() {
+  rm -rf $TMPDIR
+}
+trap cleanup EXIT
 
-GRID_ADDRESS_PARAM=
-[ -n "$GRID_URL" ] && GRID_ADDRESS_PARAM="-e jitsi_torture_grid_address=$GRID_URL"
+set -x
 
-ansible-playbook --verbose ansible/torturetest-shard-locally.yml \
--i 'somehost,' \
---extra-vars "hcv_environment=$ENVIRONMENT hcv_domain=$TEST_DOMAIN prosody_domain_name=$TEST_DOMAIN shard_name=$SHARD shard_ip_address=$SHARD_IP torture_longtest_duration=$TEST_DURATION ec2_torture_instance_count=$TORTURE_COUNT torture_longtest_only=$TORTURE_LONG" \
--e "test_subdir=$TEST_SUBDIR" \
--e "jitsi_torture_git_branch="$TORTURE_BRANCH"" \
--e "test_id=$TEST_ID" \
--e "ansible_ssh_user=$ANSIBLE_SSH_USER" \
--e "ec2_keypair=$EC2_SSH_KEYPAIR" \
--e "ec2_torture_image_id=$TORTURE_IMAGE_ID" \
--e "infra_path=$(realpath $LOCAL_PATH/../../..)" \
--e "jitsi_torture_jwt=$TOKEN" \
-$GRID_ADDRESS_PARAM \
---vault-password-file .vault-password.txt \
---tags "$DEPLOY_TAGS"
+pushd "$TMPDIR"
+
+git clone https://github.com/jitsi/jitsi-meet.git
+
+JITSI_MEET_BRANCH="$(getJitsiMeetTag $SHARD)"
+
+pushd jitsi-meet
+
+# Check if the release branch exists
+if git show-ref --verify --quiet "refs/heads/release-${JITSI_MEET_BRANCH}"; then
+  echo "Branch '${JITSI_MEET_BRANCH}' exists. Checking out..."
+  git checkout ${JITSI_MEET_BRANCH}
+else
+#  git checkout tags/${JITSI_MEET_BRANCH}
+  git checkout ${JITSI_MEET_BRANCH} # TODO: remove this line
+fi
+
+if [ -n "$JAAS_JWT_KID" ]; then
+  export IFRAME_TENANT="$(echo "${JAAS_JWT_KID}" | cut -d'/' -f1)"
+  export JWT_PRIVATE_KEY_PATH=$JAAS_SIGNING_KEY_FILE
+  export JWT_KID="${JAAS_JWT_KID}"
+  export WEBHOOKS_PROXY_URL="${WEBHOOKS_PROXY_URL}?tenant=${IFRAME_TENANT}"
+  export WEBHOOKS_PROXY_SHARED_SECRET="${JAAS_WH_SHARED_SECRET}"
+fi
+
+nvm install
+nvm use
+
+node -v
+npm -v
+echo "Start npm install"
+npm install
+echo "Done npm install"
+HEADLESS=true \
+ GRID_HOST_URL="${GRID_URL}" \
+ REMOTE_RESOURCE_PATH="/usr/share/jitsi-meet-torture/resources" \
+ ALLOW_INSECURE_CERTS=true \
+ BASE_URL="https://${TEST_DOMAIN}/70r7ur5/" \
+ MAX_INSTANCES=4 \
+ ROOM_NAME_SUFFIX="${SHARD}" \
+ npm run test-grid
+echo "Done testing"
+
+popd
+popd
+
+mv $TMPDIR/jitsi-meet/test-results ../test-results/${SHARD}
 
 if [ $? -eq 0 ]; then
-  #successful run, so keep testing
-  TORTURE_LOG_PATH="$TORTURE_PATH/torture_test.log"
-
-  ls $TORTURE_LOG_PATH >/dev/null 2>&1
-  if [ $? -eq 0 ]; then
-    grep -q "BUILD FAILURE" $TORTURE_LOG_PATH
-    if [ $? -eq 0 ]; then
-      $LOCAL_PATH/set_shard_tested.py $ENVIRONMENT $SHARD failed $BUILD_NUMBER
-      echo "Torture test \"$TORTURE_LONG\" failed."
-      exit 1
-    fi
-    #success, so mark the shard as tested
-      echo "Torture test \"$TORTURE_LONG\" passed."
-    $LOCAL_PATH/set_shard_tested.py $ENVIRONMENT $SHARD passed $BUILD_NUMBER
-  else
-    $LOCAL_PATH/set_shard_tested.py $ENVIRONMENT $SHARD failed $BUILD_NUMBER
-    echo "Torture test \"$TORTURE_LONG\" no log available."
-    exit 2
-  fi
-
+  $LOCAL_PATH/set_shard_tested.py $ENVIRONMENT $SHARD passed $BUILD_NUMBER
 else
   $LOCAL_PATH/set_shard_tested.py $ENVIRONMENT $SHARD failed $BUILD_NUMBER
   echo "Torture test ansible run failed."
   exit 1
 fi
-
