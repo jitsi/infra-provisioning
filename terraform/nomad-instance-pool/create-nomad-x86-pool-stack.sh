25350deba3cd9b7cd9b0f932decacfd7745e1603
scott boone <scott.e.boone@gmail.com>
2025-02-27 09:48:25 -0600

    increase x86 disk size to accomodate anticipated ervice need

--- terraform/nomad-instance-pool/create-nomad-x86-pool-stack.sh
+++ terraform/nomad-instance-pool/create-nomad-x86-pool-stack.sh
@@ -4,7 +4,7 @@ unset SSH_USER
 
 [ -z "$POOL_TYPE" ] && export POOL_TYPE="x86"
 [ -z "$SHAPE" ] && export SHAPE="VM.Standard.E4.Flex"
-[ -z "$DISK_IN_GBS" ] && export DISK_IN_GBS="50"
+[ -z "$DISK_IN_GBS" ] && export DISK_IN_GBS="250"
 
 LOCAL_PATH=$(dirname "${BASH_SOURCE[0]}")
 $LOCAL_PATH/create-nomad-instance-pool-stack.sh $@
