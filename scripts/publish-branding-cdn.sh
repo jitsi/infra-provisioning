2aef0ad1df14a11c8804a6638833dc3a3cd16b32
Aaron van Meerten <aaron.van.meerten@gmail.com>
2025-03-07 10:45:44 -0600

    feat(cdn): use rclone for R2 CDN publish (#827)
    * feat(ops-agent): add rclone tool
    
    * feat(cdn): use rclone to publish CDN to cloudflare
    
    * mkdir before cat

--- scripts/publish-branding-cdn.sh
+++ scripts/publish-branding-cdn.sh
@@ -28,6 +28,8 @@ fi
 
 [ -z "$CDN_R2_BUCKET" ] && CDN_R2_BUCKET="$(yq '.cdn_r2_bucket' < $CONFIG_VARS_PATH)"
 
+[ -z "$RCLONE_CONFIG_PATH" ] && RCLONE_CONFIG_PATH="$HOME/.config/rclone/rclone.conf"
+
 if [ -n "$CDN_R2_BUCKET" ]; then
     R2_SECRETS_PATH="$LOCAL_PATH/../ansible/secrets/r2-bucket.yml"
     [ -z "$VAULT_PASSWORD_FILE" ] && VAULT_PASSWORD_FILE="$LOCAL_PATH/../.vault-password.txt"
@@ -35,6 +37,17 @@ if [ -n "$CDN_R2_BUCKET" ]; then
     R2_ACCESS_KEY_ID="$(ansible-vault view $R2_SECRETS_PATH --vault-password $VAULT_PASSWORD_FILE | yq eval ".r2_access_key_id" -)"
     R2_SECRET_ACCESS_KEY="$(ansible-vault view $R2_SECRETS_PATH --vault-password $VAULT_PASSWORD_FILE | yq eval ".r2_secret_access_key" -)"
     R2_ENDPOINT_URL="$(ansible-vault view $R2_SECRETS_PATH --vault-password $VAULT_PASSWORD_FILE | yq eval ".r2_endpoint_url" -)"
+    mkdir -p "$(dirname $RCLONE_CONFIG_PATH)"
+    cat > "$RCLONE_CONFIG_PATH" <<EOF
+[default]
+type = s3
+provider = Cloudflare
+access_key_id = $R2_ACCESS_KEY_ID
+secret_access_key = $R2_SECRET_ACCESS_KEY
+endpoint = $R2_ENDPOINT_URL
+bucket_acl = private
+no_check_bucket = true
+EOF
     set -x
 fi
 
@@ -77,17 +90,13 @@ curl -O $DEB_URL
 dpkg -x $DEB_FILENAME .
 
 if [ -n "$CDN_R2_BUCKET" ]; then
-    AWS_ACCESS_KEY_ID=$R2_ACCESS_KEY_ID \
-    AWS_SECRET_ACCESS_KEY=$R2_SECRET_ACCESS_KEY \
-    AWS_DEFAULT_REGION=auto \
-    aws s3 cp --recursive usr/share/${BRANDING_NAME} s3://$CDN_R2_BUCKET/v1/_cdn/${VERSION_PREFIX}${BRANDING_COMPLETE_VERSION}/ \
-    --endpoint-url $R2_ENDPOINT_URL &
+    rclone copy usr/share/${BRANDING_NAME} default:$CDN_R2_BUCKET/v1/_cdn/${VERSION_PREFIX}${BRANDING_COMPLETE_VERSION} &
 fi
 
 aws s3 cp --recursive usr/share/${BRANDING_NAME} s3://$CDN_S3_BUCKET/${VERSION_PREFIX}${BRANDING_COMPLETE_VERSION}/ &
 
 wait
-
+rm $RCLONE_CONFIG_PATH
 s3cmd --recursive modify --add-header="Cache-Control: public, max-age=31536000" s3://$CDN_S3_BUCKET/${VERSION_PREFIX}${BRANDING_COMPLETE_VERSION}/
 s3cmd --recursive modify --add-header="Access-Control-Allow-Origin: *" s3://$CDN_S3_BUCKET/${VERSION_PREFIX}${BRANDING_COMPLETE_VERSION}/
 s3cmd --recursive modify --add-header="Cross-Origin-Resource-Policy: cross-origin" s3://$CDN_S3_BUCKET/${VERSION_PREFIX}${BRANDING_COMPLETE_VERSION}/
