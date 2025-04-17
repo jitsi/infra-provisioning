99fe365f89e25f274a8222c9944f8063fca80943
Aaron van Meerten <aaron.van.meerten@8x8.com>
2025-04-16 20:59:00 -0500

    fix(nomad-pool): explict vnic details no public ip

--- terraform/nomad-pool/nomad-pool-stack.tf
+++ terraform/nomad-pool/nomad-pool-stack.tf
@@ -126,6 +126,7 @@ resource "oci_core_instance_configuration" "oci_instance_configuration" {
       }
 
       create_vnic_details {
+        assign_public_ip = false
         subnet_id = var.pool_subnet_ocid
         nsg_ids = [var.security_group_id]
       }
