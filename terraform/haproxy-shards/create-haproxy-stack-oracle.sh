46a6bc81448647ec07850e8642f91a9d4b28f237
Aaron van Meerten <aaron.van.meerten@8x8.com>
2025-05-14 16:04:34 -0500

    feat(haproxy): move to noble base image (#880)

--- terraform/haproxy-shards/create-haproxy-stack-oracle.sh
+++ terraform/haproxy-shards/create-haproxy-stack-oracle.sh
@@ -189,7 +189,7 @@ fi
 [ -z "$S3_STATE_KEY" ] && S3_STATE_KEY="$ENVIRONMENT/haproxy-components/terraform.tfstate"
 
 [ -z "$BASE_IMAGE_TYPE" ] && BASE_IMAGE_TYPE="$HAPROXY_BASE_IMAGE_TYPE"
-[ -z "$BASE_IMAGE_TYPE" ] && BASE_IMAGE_TYPE="JammyBase"
+[ -z "$BASE_IMAGE_TYPE" ] && BASE_IMAGE_TYPE="NobleBase"
 
 arch_from_shape $SHAPE
 
