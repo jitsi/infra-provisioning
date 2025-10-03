cb80abbebc1efd0499ef77be4f9cdc98ba19819c
Aaron van Meerten <aaron.van.meerten@8x8.com>
2025-10-03 08:59:11 +0200

    fix: hcvlib shard region detection

--- scripts/hcvlib.py
+++ scripts/hcvlib.py
@@ -1587,7 +1587,7 @@ def find_asg_by_tags(region,environment,role=False,grid=False,grid_role=False,re
 def shard_region_from_name(shard_name, map_to_aws=True):
     shard_region = ''
     check_oracle_map = False
-    shard_signs = ['-s','-global-','-remote-','-local-']
+    shard_signs = ['-s','-global-','-remote-','-local-','Group']
     if shard_name and len([ x for x in shard_signs if x in shard_name ])>0:
         shard_pieces = shard_name.split('-')
         shard_number = shard_pieces.pop()
