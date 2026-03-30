fb628df892af03b83ba95d4abe843b7bf4d51c78
scott boone <scott.e.boone@gmail.com>
2026-03-30 15:10:33 -0500

    testing, DO NOT MERGE

--- scripts/delete-shard-custom-jvbs-oracle.sh
+++ scripts/delete-shard-custom-jvbs-oracle.sh
@@ -83,10 +83,10 @@ if [ "$getGroupHttpCode" == 404 ]; then
   findGroup
   if [ "$getGroupHttpCode" == 404 ]; then
     echo "No group $GROUP_NAME found at $AUTOSCALER_URL. Assuming no more work to do"
-    exit 0
+    #exit 0
   elif [ "$getGroupHttpCode" == 000 ]; then
     echo "Local autoscaler not present for $GROUP_NAME. Assuming no more work to do"
-    exit 0
+    #exit 0
   elif [ "$getGroupHttpCode" == 200 ]; then
     echo "Group $GROUP_NAME was found in the autoscaler"
     export CLOUD_PROVIDER="$(echo "$instanceGroupDetails" | jq -r ."instanceGroup.cloud")"
@@ -129,74 +129,74 @@ function delGroup() {
 
 # Scale down JVB custom group to 0
 ##################################
-$LOCAL_PATH/custom-autoscaler-update-desired-values.sh
-RESULT=$?
-
-if [ "$RESULT" -eq 0 ]; then
-  # Wait a while until the JVBs gracefully terminate
-  ##################################
-  export EXPECTED_COUNT=0
-  export CHECK_SCALE_UP="false"
-  $LOCAL_PATH/check-jvb-count-custom-autoscaler-oracle.sh
-
-  # Force terminate remaining instances if there are any left
-  ##################################
-  echo "Force terminating remaining instances on group $GROUP_NAME, if any"
-  instanceGroupGetResponse=$(curl -s -w "\n %{http_code}" -X GET \
-        "$AUTOSCALER_URL"/groups/"$GROUP_NAME"/report \
-        -H "Authorization: Bearer $TOKEN")
-
-  GROUP_REPORT_STATUS_CODE=$(tail -n1 <<<"$instanceGroupGetResponse" | sed 's/[^0-9]*//g') # get the last line
-  GROUP_REPORT_VALUE=$(sed '$ d' <<<"$instanceGroupGetResponse")                 # get all but the last line which contains the status code
-
-  if [ "$GROUP_REPORT_STATUS_CODE" == 200 ]; then
-    GROUP_REPORT_INSTANCES="$(echo "$GROUP_REPORT_VALUE" | jq '.groupReport.instances | map(select(.cloudStatus!="SHUTDOWN"))')"
-    INSTANCES=$(echo "$GROUP_REPORT_INSTANCES" | jq -r '.[].instanceId')
-    for INSTANCE_ID in $INSTANCES; do
-      if (echo "$INSTANCE_ID" | grep -q "/dispatch-"); then
-        echo "Terminating nomad instance $INSTANCE_ID"
-        $LOCAL_PATH/nomad.sh job stop $INSTANCE_ID
-      else
-        echo "Terminating JVB instance $INSTANCE_ID"
-        oci --region $ORACLE_REGION compute instance terminate --force --instance-id $INSTANCE_ID
-      fi
-    done
-  else
-    echo "Failed to get remaining group report instances. Please retry the script"
-    exit 220
-  fi
-
-  DELETE_FAILED=true
-# by default retry for up to 6 hours 
-  DELETE_RETRIES=180 # 30 retries in an hour (every 2 mins), 6 hours
-  DELETE_RETRY=0
-  while $DELETE_FAILED; do
-    delGroup
-    if [ $? -eq 0 ]; then
-      DELETE_FAILED=false
-    else
-      DELETE_RETRY=$((DELETE_RETRY+1))
-      if [[ $DELETE_RETRY -gt $DELETE_RETRIES ]]; then
-        echo "Retries exhausted, failed to delete many times"
-        DELETE_FAILED=false
-      else
-        # failed, but within our retry limit so sleep and try again in 5 mins
-        sleep 120
-      fi
-    fi
-  done
-elif [ "$RESULT" -eq 230 ]; then
-  echo "Group $GROUP_NAME not found, but continuing to check if there are remaining instance configurations to be deleted"
-elif [ "$RESULT" -gt 0 ]; then
-  echo "Failed setting min=max=desired=0 on the group $GROUP_NAME. Exiting.."
-  exit 221
-fi
-
-# Delete instance configuration(s) for that shard
-##################################
-if [[ "$CLOUD_PROVIDER" == "oracle" ]]; then
-  $LOCAL_PATH/../terraform/create-jvb-instance-configuration/delete-jvb-instance-configuration.sh
-fi
+#$LOCAL_PATH/custom-autoscaler-update-desired-values.sh
+#RESULT=$?
+#
+#if [ "$RESULT" -eq 0 ]; then
+#  # Wait a while until the JVBs gracefully terminate
+#  ##################################
+#  export EXPECTED_COUNT=0
+#  export CHECK_SCALE_UP="false"
+#  $LOCAL_PATH/check-jvb-count-custom-autoscaler-oracle.sh
+#
+#  # Force terminate remaining instances if there are any left
+#  ##################################
+#  echo "Force terminating remaining instances on group $GROUP_NAME, if any"
+#  instanceGroupGetResponse=$(curl -s -w "\n %{http_code}" -X GET \
+#        "$AUTOSCALER_URL"/groups/"$GROUP_NAME"/report \
+#        -H "Authorization: Bearer $TOKEN")
+#
+#  GROUP_REPORT_STATUS_CODE=$(tail -n1 <<<"$instanceGroupGetResponse" | sed 's/[^0-9]*//g') # get the last line
+#  GROUP_REPORT_VALUE=$(sed '$ d' <<<"$instanceGroupGetResponse")                 # get all but the last line which contains the status code
+#
+#  if [ "$GROUP_REPORT_STATUS_CODE" == 200 ]; then
+#    GROUP_REPORT_INSTANCES="$(echo "$GROUP_REPORT_VALUE" | jq '.groupReport.instances | map(select(.cloudStatus!="SHUTDOWN"))')"
+#    INSTANCES=$(echo "$GROUP_REPORT_INSTANCES" | jq -r '.[].instanceId')
+#    for INSTANCE_ID in $INSTANCES; do
+#      if (echo "$INSTANCE_ID" | grep -q "/dispatch-"); then
+#        echo "Terminating nomad instance $INSTANCE_ID"
+#        $LOCAL_PATH/nomad.sh job stop $INSTANCE_ID
+#      else
+#        echo "Terminating JVB instance $INSTANCE_ID"
+#        oci --region $ORACLE_REGION compute instance terminate --force --instance-id $INSTANCE_ID
+#      fi
+#    done
+#  else
+#    echo "Failed to get remaining group report instances. Please retry the script"
+#    exit 220
+#  fi
+#
+#  DELETE_FAILED=true
+## by default retry for up to 6 hours 
+#  DELETE_RETRIES=180 # 30 retries in an hour (every 2 mins), 6 hours
+#  DELETE_RETRY=0
+#  while $DELETE_FAILED; do
+#    delGroup
+#    if [ $? -eq 0 ]; then
+#      DELETE_FAILED=false
+#    else
+#      DELETE_RETRY=$((DELETE_RETRY+1))
+#      if [[ $DELETE_RETRY -gt $DELETE_RETRIES ]]; then
+#        echo "Retries exhausted, failed to delete many times"
+#        DELETE_FAILED=false
+#      else
+#        # failed, but within our retry limit so sleep and try again in 5 mins
+#        sleep 120
+#      fi
+#    fi
+#  done
+#elif [ "$RESULT" -eq 230 ]; then
+#  echo "Group $GROUP_NAME not found, but continuing to check if there are remaining instance configurations to be deleted"
+#elif [ "$RESULT" -gt 0 ]; then
+#  echo "Failed setting min=max=desired=0 on the group $GROUP_NAME. Exiting.."
+#  exit 221
+#fi
+#
+## Delete instance configuration(s) for that shard
+###################################
+#if [[ "$CLOUD_PROVIDER" == "oracle" ]]; then
+#  $LOCAL_PATH/../terraform/create-jvb-instance-configuration/delete-jvb-instance-configuration.sh
+#fi
 
 if [[ "$CLOUD_PROVIDER" == "nomad" ]]; then
   # find all running jobs matching prefix and stop them
